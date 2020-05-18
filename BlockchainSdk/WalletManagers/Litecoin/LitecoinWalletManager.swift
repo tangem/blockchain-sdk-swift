752775be7e568cae6a684e8f579aa2166e982edb
Alexander Osokin <aosokin@tangem.com>
2020-05-18 16:44:33 +0300

    BSS-4 LTC fee

--- BlockchainSdk/WalletManagers/Litecoin/LitecoinWalletManager.swift
+++ BlockchainSdk/WalletManagers/Litecoin/LitecoinWalletManager.swift
@@ -10,11 +10,7 @@ import Foundation
 import Combine
 
 class LitecoinWalletManager: BitcoinWalletManager {
-    @available(iOS 13.0, *)
-    override func getFee(amount: Amount, source: String, destination: String) -> AnyPublisher<[Amount], Error> {
-        let fee = Amount(with: .litecoin, address: source, value: Decimal(0.00001))
-        return Just([fee])
-            .setFailureType(to: Error.self)
-            .eraseToAnyPublisher()
+    override var relayFee: Decimal? {
+        return Decimal(0.00001)
     }
 }
