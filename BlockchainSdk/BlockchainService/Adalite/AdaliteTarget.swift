076020a030d71a46bd9575bb61968d74b61f27f9
Andrew Son <ason@tangem.com>
2021-03-12 21:23:48 +0500

    BSS-84 Fix for adalite base urls

--- BlockchainSdk/BlockchainService/Adalite/AdaliteTarget.swift
+++ BlockchainSdk/BlockchainService/Adalite/AdaliteTarget.swift
@@ -10,8 +10,8 @@ import Foundation
 import Moya
 
 enum AdaliteUrl: String {
-    case main = "https://explorer2.adalite.io/12345"
-    case reserve = "https://nodes.southeastasia.cloudapp.azure.com/"
+    case main = "https://explorer2.adalite.io"
+    case reserve = "https://nodes.southeastasia.cloudapp.azure.com"
 }
 
 enum AdaliteTarget: TargetType {
