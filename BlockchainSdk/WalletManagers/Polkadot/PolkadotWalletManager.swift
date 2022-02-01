9adcd5d635c67639fdbef70dffed0e6d96b87e9f
Andrey Chukavin <megadestroyer007@gmail.com>
2022-02-01 17:13:06 +0400

    BSS-185 Sending/signing transactions

--- BlockchainSdk/WalletManagers/Polkadot/PolkadotWalletManager.swift
+++ BlockchainSdk/WalletManagers/Polkadot/PolkadotWalletManager.swift
@@ -29,11 +29,35 @@ extension PolkadotWalletManager: TransactionSender {
     }
     
     func send(_ transaction: Transaction, signer: TransactionSigner) -> AnyPublisher<Void, Error> {
-        .emptyFail        
+        networkService
+            .blockchainMeta(for: transaction.sourceAddress)
+            .flatMap { meta in
+                self.sign(meta: meta, transaction: transaction, signer: signer)
+            }
+            .flatMap { image in
+                self.networkService.submitExtrinsic(data: image)
+            }
+            .eraseToAnyPublisher()
     }
     
     func getFee(amount: Amount, destination: String) -> AnyPublisher<[Amount], Error> {
         .emptyFail
+    }
+    
+    private func sign(meta: PolkadotBlockchainMeta, transaction: Transaction, signer: TransactionSigner) -> AnyPublisher<Data, Error> {
+        let x = Just(())
+            .tryMap { _ in
+                try self.txBuilder.buildForSign(transaction: transaction, walletAmount: 0, isEstimated: false, meta: meta)
+            }
+            .flatMap { preImage in
+                signer.sign(hash: preImage, cardId: self.wallet.cardId, walletPublicKey: self.wallet.publicKey)
+            }
+            .tryMap { signature in
+                try self.txBuilder.buildForSend(meta: meta, transaction: transaction, signature: signature)
+            }
+            .eraseToAnyPublisher()
+        
+        return x
     }
 }
 
