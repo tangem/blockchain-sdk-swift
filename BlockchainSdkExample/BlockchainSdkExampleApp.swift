876f1980b9fb27d9459fd6844e605899c9fcd4ba
Andrey Chukavin <megadestroyer007@gmail.com>
2022-03-11 10:30:24 +0400

    BSS-178 Use app delegate instead of scene delegate.

--- BlockchainSdkExample/BlockchainSdkExampleApp.swift
+++ BlockchainSdkExample/BlockchainSdkExampleApp.swift
@@ -9,25 +9,20 @@
 import UIKit
 import SwiftUI
 
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-}
-
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     let model = BlockchainSdkExampleViewModel()
     
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let contentView = BlockchainSdkExampleView()
             .environmentObject(model)
+        
+        let window = UIWindow()
+        window.rootViewController = UIHostingController(rootView: contentView)
+        self.window = window
+        window.makeKeyAndVisible()
 
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        return true
     }
 }
