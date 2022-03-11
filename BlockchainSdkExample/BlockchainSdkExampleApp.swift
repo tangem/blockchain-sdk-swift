9d62c1c88c938e78b71c7886aac93f38a21a855b
Andrey Chukavin <megadestroyer007@gmail.com>
2022-03-11 09:42:03 +0400

    BSS-178 Use scene delegate

--- BlockchainSdkExample/BlockchainSdkExampleApp.swift
+++ BlockchainSdkExample/BlockchainSdkExampleApp.swift
@@ -6,16 +6,28 @@
 //  Copyright © 2022 Tangem AG. All rights reserved.
 //
 
+import UIKit
 import SwiftUI
 
-@main
-struct BlockchainSdkExampleApp: App {
-    var body: some Scene {
-        WindowGroup {
-            BlockchainSdkExampleView()
-                .onAppear {
-                    UIScrollView.appearance().keyboardDismissMode = .onDrag
-                }
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    let model = BlockchainSdkExampleViewModel()
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = BlockchainSdkExampleView()
+            .environmentObject(model)
+
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
         }
     }
 }
