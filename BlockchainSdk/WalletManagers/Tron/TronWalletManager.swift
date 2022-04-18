421d5a99b4948581d5ea94480f9f69d150c07a4f
Andrey Chukavin <megadestroyer007@gmail.com>
2022-04-18 11:24:33 +0400

    BSS-209 Updating amount

--- BlockchainSdk/WalletManagers/Tron/TronWalletManager.swift
+++ BlockchainSdk/WalletManagers/Tron/TronWalletManager.swift
@@ -23,9 +23,17 @@ class TronWalletManager: BaseManager, WalletManager {
     func update(completion: @escaping (Result<Void, Error>) -> Void) {
         cancellable = networkService.getAccount(for: wallet.address)
             .sink {
+                switch $0 {
+                case .failure(let error):
+                    self.wallet.clearAmounts()
+                    completion(.failure(error))
+                case .finished:
+                    completion(.success(()))
+                }
                 print($0)
-            } receiveValue: {
+            } receiveValue: { [unowned self] in
                 print($0)
+                self.updateWallet($0)
             }
     }
     
@@ -35,6 +43,11 @@ class TronWalletManager: BaseManager, WalletManager {
     
     func getFee(amount: Amount, destination: String) -> AnyPublisher<[Amount], Error> {
         .anyFail(error: WalletError.empty)
+    }
+    
+    private func updateWallet(_ response: TronGetAccountResponse) {
+        let blockchain = wallet.blockchain
+        wallet.add(amount: Amount(with: blockchain, value: Decimal(response.balance) / blockchain.decimalValue))
     }
 }
 
