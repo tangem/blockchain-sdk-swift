c1371201411e9f0df318677c49431c70d4db8c9d
Andrey Chukavin <megadestroyer007@gmail.com>
2022-04-21 15:22:55 +0400

    BSS-209 Added Shasta. Use trongrid

--- BlockchainSdk/WalletManagers/Tron/TronNetwork.swift
+++ BlockchainSdk/WalletManagers/Tron/TronNetwork.swift
@@ -10,14 +10,17 @@ import Foundation
 
 enum TronNetwork {
     case mainnet
+    case shasta
     case nile
     
     var url: URL {
         switch self {
         case .mainnet:
             return URL(string: "https://api.trongrid.io")!
+        case .shasta:
+            return URL(string: "https://api.shasta.trongrid.io/")!
         case .nile:
-            return URL(string: "https://api.nileex.io")!
+            return URL(string: "https://nile.trongrid.io")!
         }
     }
 }
