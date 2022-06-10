08d5b7cf1c2d58705b4093248dda5c8d370ac709
Pavel Grechikhin <pav.gre4ixin@yandex.ru>
2022-06-11 02:52:16 +0400

    BSS-229 Add comments

--- BlockchainSdk/WalletManagers/Near/NearTransactionBuilder.swift
+++ BlockchainSdk/WalletManagers/Near/NearTransactionBuilder.swift
@@ -10,6 +10,16 @@ import Foundation
 
 class NearTransactionBuilder {
     func build(from tx: Transaction) {
-        
     }
+}
+
+fileprivate struct NearTransaction: Encodable {
+    let signerId: String
+    let signerPublicKey: [UInt8]
+    let receiverId: String
+    /// Call Access view list key
+    let nonceForPublicKey: UUID = UUID()
+    let action: String = "Transfer"
+    /// Call Last block request, decode from base58 last hash
+    let blockHash: [UInt8]
 }
