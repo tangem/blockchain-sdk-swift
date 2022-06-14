b26d700d27d12057bb557697fdd974c3f0e9bbda
Andrey Chukavin <megadestroyer007@gmail.com>
2022-06-14 10:50:21 +0400

    IOS-1614 Replaced TangemSdk extension with CommonSigner

--- BlockchainSdk/Extensions/TangemSdk+TransactionSigner.swift
+++ BlockchainSdk/Extensions/TangemSdk+TransactionSigner.swift
@@ -11,44 +11,52 @@ import TangemSdk
 import Combine
 
 @available(iOS 13.0, *)
-extension TangemSdk: TransactionSigner {
-    public func sign(hashes: [Data], walletPublicKey: Wallet.PublicKey) -> AnyPublisher<[Data], Error> {
-        let future = Future<[Data], Error> {[weak self] promise in
-            guard let self = self else {
-                promise(.failure(WalletError.empty))
-                return
-            }
-            
-            self.sign(hashes: hashes, walletPublicKey: walletPublicKey.seedKey,
-                      derivationPath: walletPublicKey.derivationPath) { signResult in
-                switch signResult {
-                case .success(let response):
-                    promise(.success(response.signatures))
-                case .failure(let error):
-                    promise(.failure(error))
-                }
-            }
-        }
-        return AnyPublisher(future)
+public class CommonSigner {
+    public var initialMessage: Message?
+    public var cardId: String?
+    
+    private let sdk: TangemSdk
+    
+    public init(sdk: TangemSdk, initialMessage: Message? = nil, cardId: String? = nil) {
+        self.sdk = sdk
+        self.initialMessage = initialMessage
+        self.cardId = cardId
     }
+}
 
-    public func sign(hash: Data, walletPublicKey: Wallet.PublicKey) -> AnyPublisher<Data, Error> {
-        let future = Future<Data, Error> {[weak self] promise in
-            guard let self = self else {
-                promise(.failure(WalletError.empty))
-                return
-            }
-            
-            self.sign(hash: hash, walletPublicKey: walletPublicKey.seedKey,
-                      derivationPath: walletPublicKey.derivationPath) { signResult in
-                switch signResult {
-                case .success(let response):
-                    promise(.success(response.signature))
-                case .failure(let error):
-                    promise(.failure(error))
+extension CommonSigner: TransactionSigner {
+    public func sign(hashes: [Data], walletPublicKey: Wallet.PublicKey) -> AnyPublisher<[Data], Error> {
+        Deferred {
+            Future { [weak self] promise in
+                guard let self = self else {
+                    promise(.failure(WalletError.empty))
+                    return
+                }
+                
+                return self.sdk.sign(
+                    hashes: hashes,
+                    walletPublicKey: walletPublicKey.seedKey,
+                    cardId: self.cardId,
+                    derivationPath: walletPublicKey.derivationPath,
+                    initialMessage: self.initialMessage
+                ) { signResult in
+                    switch signResult {
+                    case .success(let response):
+                        promise(.success(response.signatures))
+                    case .failure(let error):
+                        promise(.failure(error))
+                    }
                 }
             }
         }
-        return AnyPublisher(future)
+        .eraseToAnyPublisher()
+    }
+    
+    public func sign(hash: Data, walletPublicKey: Wallet.PublicKey) -> AnyPublisher<Data, Error> {
+        sign(hashes: [hash], walletPublicKey: walletPublicKey)
+            .map {
+                $0.first ?? Data()
+            }
+            .eraseToAnyPublisher()
     }
 }
