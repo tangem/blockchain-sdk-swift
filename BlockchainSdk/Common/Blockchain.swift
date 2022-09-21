f44b2125e2bd70c6108c411cf2df4d763c9d9a34
Andrey Chukavin <megadestroyer007@gmail.com>
2022-09-21 17:36:11 +0300

    IOS-2137 Swapped PoW RPCs, added address into explorer URL

--- BlockchainSdk/Common/Blockchain.swift
+++ BlockchainSdk/Common/Blockchain.swift
@@ -296,9 +296,9 @@ extension Blockchain {
             }
         case .ethereumPoW:
             if isTestnet {
-                return [URL(string: "https://mainnet.ethereumpow.org")!]
-            } else {
                 return [URL(string: "https://iceberg.ethereumpow.org")!]
+            } else {
+                return [URL(string: "https://mainnet.ethereumpow.org")!]
             }
         case .ethereumFair:
             return [URL(string: "https://rpc.etherfair.org")!]
@@ -719,12 +719,12 @@ extension Blockchain {
             return URL(string: "https://blockscout.com/etc/\(network)/address/\(address)/transactions")!
         case .ethereumPoW(let testnet):
             if testnet {
-                return URL(string: "http://iceberg.ethwscan.com")
+                return URL(string: "http://iceberg.ethwscan.com/address/\(address)")
             } else {
-                return URL(string: "https://mainnet.ethwscan.com")
+                return URL(string: "https://mainnet.ethwscan.com/address/\(address)")
             }
         case .ethereumFair:
-            return URL(string: "https://explorer.etherfair.org")
+            return URL(string: "https://explorer.etherfair.org/address/\(address)")
         case .litecoin:
             return URL(string: "https://blockchair.com/litecoin/address/\(address)")
         case .rsk:
