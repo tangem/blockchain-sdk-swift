a075f6344dda86b451c8f0adbeb66ecd37c1f1a9
Andrey Chukavin <megadestroyer007@gmail.com>
2022-12-24 15:41:25 +0300

    IOS-2360 Refactoring

--- BlockchainSdk/BlockchainService/BlockBook/BlockBookProvider.swift
+++ BlockchainSdk/BlockchainService/BlockBook/BlockBookProvider.swift
@@ -29,113 +29,17 @@ class BlockBookProvider: BitcoinNetworkProvider {
     func getInfo(address: String) -> AnyPublisher<BitcoinResponse, Error> {
         Publishers
             .Zip(addressData(walletAddress: address), unspentTxData(walletAddress: address))
-            .tryMap { (addressResponse, unspentTxResponse) in
+            .tryMap { [weak self] (addressResponse, unspentTxResponse) in
+                guard let self else {
+                    throw WalletError.empty
+                }
+                
                 let transactions = addressResponse.transactions ?? []
-                
-                let outputScript = transactions
-                    .compactMap { transaction in
-                        transaction.vout.first {
-                            $0.addresses.contains(address)
-                        }
-                    }
-                    .map { vout in
-                        vout.hex
-                    }
-                    .first
-                
-                let unspentOutputs: [BitcoinUnspentOutput] = transactions
-                    .filter {
-                        $0.confirmations > 0
-                    }
-                    .map { transaction in
-                        let utxos = transaction.vout.filter { $0.addresses.contains(address) && $0.spent == nil }
-
-                        return utxos.compactMap {
-                            guard let outputScript = outputScript else {
-                                return nil
-                            }
-
-                            return BitcoinUnspentOutput(
-                                transactionHash: transaction.txid,
-                                outputIndex: $0.n,
-                                amount: UInt64($0.value) ?? 0,
-                                outputScript: outputScript
-                            )
-                        }
-                    }
-                    .reduce([], +)
-                
-                let pendingTxRefs: [PendingTransaction] = transactions
-                    .filter {
-                        $0.confirmations == 0
-                    }
-                    .compactMap { tx -> PendingTransaction? in
-                        let source: String?
-                        let destination: String?
-                        let value: Decimal?
-                        let isIncoming: Bool
-                        
-                        if tx.vin.contains(where: { $0.addresses.contains(address) }), let destinationUtxo = tx.vout.first(where: { !$0.addresses.contains(address) }) {
-                            isIncoming = false
-                            destination = destinationUtxo.addresses.first
-                            source = address
-                            value = Decimal(string: destinationUtxo.value)
-                        } else if let txDestination = tx.vout.first(where: { $0.addresses.contains(address) }), !tx.vin.contains(where: { $0.addresses.contains(address) }), let txSource = tx.vin.first {
-                            isIncoming = true
-                            destination = address
-                            source = txSource.addresses.first
-                            value = Decimal(string: txDestination.value)
-                        } else {
-                            return nil
-                        }
-                        
-                        let bitcoinInputs: [BitcoinInput] = tx.vin.compactMap { input -> BitcoinInput? in
-                            guard
-                                let address = input.addresses.first,
-                                let value = UInt64(input.value ?? ""),
-                                let outputIndex = input.vout
-                            else {
-                                return nil
-                            }
-                            
-                            return BitcoinInput(
-                                sequence: input.n,
-                                address: address,
-                                outputIndex: outputIndex,
-                                outputValue: value,
-                                prevHash: input.txid
-                            )
-                        }
-                        
-                        let fee: Decimal?
-                        if let fetchedFees = Decimal(string: tx.fees) {
-                            fee = fetchedFees / self.blockchain.decimalValue
-                        } else {
-                            fee = nil
-                        }
-                        
-                        guard
-                            let source,
-                            let destination,
-                            let value
-                        else {
-                            return nil
-                        }
-                        
-                        let date = Date(timeIntervalSince1970: Double(tx.blockTime))
-                        
-                        return PendingTransaction(hash: tx.txid,
-                                                  destination: destination,
-                                                  value: value / self.blockchain.decimalValue,
-                                                  source: source,
-                                                  fee: fee,
-                                                  date: date,
-                                                  isIncoming: isIncoming,
-                                                  transactionParams: BitcoinTransactionParams(inputs: bitcoinInputs))
-                    }
                 
                 let balance = (Decimal(string: addressResponse.balance) ?? 0) / self.blockchain.decimalValue
                 let hasUnconfirmed = addressResponse.unconfirmedTxs != 0
+                let pendingTxRefs = self.pendingTransactions(from: transactions, address: address)
+                let unspentOutputs = self.unspentOutputs(from: unspentTxResponse, transactions: transactions, address: address)
                 
                 return BitcoinResponse(
                     balance: balance,
@@ -207,5 +111,98 @@ class BlockBookProvider: BitcoinNetworkProvider {
     
     private func target(for request: BlockBookTarget.Request) -> BlockBookTarget {
         BlockBookTarget(request: request, serviceProvider: serviceProvider, blockchain: blockchain)
+    }
+    
+    private func pendingTransactions(from transactions: [BlockBookAddressResponse.Transaction], address: String) -> [PendingTransaction] {
+        transactions
+            .filter {
+                $0.confirmations == 0
+            }
+            .compactMap { tx -> PendingTransaction? in
+                let source: String?
+                let destination: String?
+                let value: Decimal?
+                let isIncoming: Bool
+                
+                if tx.vin.contains(where: { $0.addresses.contains(address) }), let destinationUtxo = tx.vout.first(where: { !$0.addresses.contains(address) }) {
+                    isIncoming = false
+                    destination = destinationUtxo.addresses.first
+                    source = address
+                    value = Decimal(string: destinationUtxo.value)
+                } else if let txDestination = tx.vout.first(where: { $0.addresses.contains(address) }), !tx.vin.contains(where: { $0.addresses.contains(address) }), let txSource = tx.vin.first {
+                    isIncoming = true
+                    destination = address
+                    source = txSource.addresses.first
+                    value = Decimal(string: txDestination.value)
+                } else {
+                    return nil
+                }
+                
+                let bitcoinInputs: [BitcoinInput] = tx.vin.compactMap { input -> BitcoinInput? in
+                    guard
+                        let address = input.addresses.first,
+                        let value = UInt64(input.value ?? ""),
+                        let outputIndex = input.vout
+                    else {
+                        return nil
+                    }
+                    
+                    return BitcoinInput(
+                        sequence: input.n,
+                        address: address,
+                        outputIndex: outputIndex,
+                        outputValue: value,
+                        prevHash: input.txid
+                    )
+                }
+                
+                guard
+                    let fetchedFees = Decimal(string: tx.fees),
+                    let source,
+                    let destination,
+                    let value
+                else {
+                    return nil
+                }
+                
+                let date = Date(timeIntervalSince1970: Double(tx.blockTime))
+                
+                return PendingTransaction(hash: tx.txid,
+                                          destination: destination,
+                                          value: value / self.blockchain.decimalValue,
+                                          source: source,
+                                          fee: fetchedFees / self.blockchain.decimalValue,
+                                          date: date,
+                                          isIncoming: isIncoming,
+                                          transactionParams: BitcoinTransactionParams(inputs: bitcoinInputs))
+            }
+    }
+    
+    private func unspentOutputs(from utxos: [BlockBookUnspentTxResponse], transactions: [BlockBookAddressResponse.Transaction], address: String) -> [BitcoinUnspentOutput] {
+        let outputScript = transactions
+            .compactMap { transaction in
+                transaction.vout.first {
+                    $0.addresses.contains(address)
+                }
+            }
+            .map { vout in
+                vout.hex
+            }
+            .first
+        
+        guard let outputScript = outputScript else {
+            return []
+        }
+        
+        return utxos.compactMap {
+            guard let value = UInt64($0.value) else { return nil}
+            
+            return BitcoinUnspentOutput(
+                transactionHash: $0.txid,
+                outputIndex: $0.vout,
+                amount: value,
+                outputScript: outputScript
+            )
+        }
     }
 }
