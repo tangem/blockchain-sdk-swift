c0e502f6e42f93d4a70fc4a3ce0b2a30b8ab2b1f
Alexander Osokin <tureckiy@gmail.com>
2023-01-16 21:38:14 +0500

    Merge pull request #206 from tangem/IOS-2860_xpub
    IOS-2860 xpub

---
+++ BlockchainSdk/Common/Address/Xpub/XpubVersion.swift
@@ -0,0 +1,14 @@
+//
+//  XpubVersion.swift
+//  BlockchainSdk
+//
+//  Created by Alexander Osokin on 13.01.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+public enum XpubVersion: UInt32 {
+    case mainnet = 0x0488b21e
+    case testnet = 0x043587cf
+}
