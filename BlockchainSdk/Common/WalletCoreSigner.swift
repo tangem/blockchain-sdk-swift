8e2b85e0ac6d8791e0e3a91e486ad5f00d865981
Andrey Chukavin <megadestroyer007@gmail.com>
2023-02-03 17:57:43 +0200

    IOS-2951 Added wallet core signer

---
+++ BlockchainSdk/Common/WalletCoreSigner.swift
@@ -0,0 +1,49 @@
+//
+//  WalletCoreSigner.swift
+//  BlockchainSdk
+//
+//  Created by Andrey Chukavin on 03.02.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Combine
+
+import TangemSdk
+import WalletCore
+
+public class WalletCoreSigner: Signer {
+    private let sdkSigner: TransactionSigner
+    private let publicKey: Wallet.PublicKey
+    
+    private var signSubscription: AnyCancellable?
+    
+    public init(sdkSigner: TransactionSigner, publicKey: Wallet.PublicKey) {
+        self.sdkSigner = sdkSigner
+        self.publicKey = publicKey
+    }
+    
+    public func sign(_ data: Data) -> Data {
+        var signedData: Data?
+        
+        let operation = BlockOperation { [weak self] in
+            guard let self else { return }
+            
+            let group = DispatchGroup()
+            group.enter()
+            
+            self.signSubscription = self.sdkSigner.sign(hash: data, walletPublicKey: self.publicKey)
+                .sink { _ in
+                    group.leave()
+                } receiveValue: { data in
+                    signedData = data
+                }
+            
+            group.wait()
+        }
+        
+        operation.start()
+        operation.waitUntilFinished()
+
+        return signedData!
+    }
+}
