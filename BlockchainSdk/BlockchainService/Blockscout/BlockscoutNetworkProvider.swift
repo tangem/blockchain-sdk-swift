ce87982a4a67697faa65aee75a404aa671f276af
Andrew Son <andoran90@gmail.com>
2023-02-14 12:19:25 +0500

    IOS-2975: Error mapping for blockscout api

--- BlockchainSdk/BlockchainService/Blockscout/BlockscoutNetworkProvider.swift
+++ BlockchainSdk/BlockchainService/Blockscout/BlockscoutNetworkProvider.swift
@@ -19,9 +19,20 @@ class BlockscoutNetworkProvider {
     func loadTransactionHistory(for address: String) -> AnyPublisher<[BlockscoutTransaction], Error> {
         return networkProvider.requestPublisher(.transactionHistory(address: address))
             .filterSuccessfulStatusAndRedirectCodes()
-            .map(BlockscoutResponse<[BlockscoutTransaction]>.self)
+            .tryMap { response in
+                let jsonDecoder = JSONDecoder()
+                // When error occurs on the API side it returns null in the result field and puts error message
+                // to the message. So we need to check if result is not nil, if so - return result, otherwise
+                // throw received in `message` error
+                let decodedResponse = try jsonDecoder.decode(BlockscoutResponse<[BlockscoutTransaction]?>.self, from: response.data)
+                
+                if let result = decodedResponse.result {
+                    return result
+                }
+                
+                throw decodedResponse.message
+            }
             .mapError { $0 as Error }
-            .map { $0.result }
             .eraseToAnyPublisher()
     }
 }
