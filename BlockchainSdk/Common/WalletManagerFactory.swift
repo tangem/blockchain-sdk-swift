f0670ba39a47e832f4a6f7a79190a32f09c71a19
Andrey Chukavin <megadestroyer007@gmail.com>
2023-02-15 17:04:26 +0200

    IOS-3020 Removed RPCEndpoint

--- BlockchainSdk/Common/WalletManagerFactory.swift
+++ BlockchainSdk/Common/WalletManagerFactory.swift
@@ -214,7 +214,7 @@ public class WalletManagerFactory {
             )!
             
             if case .optimism = blockchain {
-                manager = OptimismWalletManager(wallet: wallet, rpcURL: endpoints[0].url)
+                manager = OptimismWalletManager(wallet: wallet, rpcURL: endpoints[0])
             } else {
                 manager = EthereumWalletManager(wallet: wallet)
             }
@@ -235,14 +235,8 @@ public class WalletManagerFactory {
                 let chainId = blockchain.chainId!
                 
                 let jsonRpcProviders = endpoints.map {
-                    var additionalHeaders: [String: String] = [:]
-                    if let apiKeyHeaderName = $0.apiKeyHeaderName, let apiKeyHeaderValue = $0.apiKeyHeaderValue {
-                        additionalHeaders[apiKeyHeaderName] = apiKeyHeaderValue
-                    }
-                    
                     return EthereumJsonRpcProvider(
-                        url: $0.url,
-                        additionalHeaders: additionalHeaders,
+                        url: $0,
                         configuration: networkProviderConfiguration
                     )
                 }
