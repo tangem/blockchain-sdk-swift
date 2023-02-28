b13d98b26c082b43ac6a49a25ac9d3596091df98
Andrey Chukavin <megadestroyer007@gmail.com>
2023-02-28 13:26:46 +0200

    IOS-3120 Calculating dogecoin fee according to their spec

--- BlockchainSdk/WalletManagers/Dogecoin/DogecoinWalletManager.swift
+++ BlockchainSdk/WalletManagers/Dogecoin/DogecoinWalletManager.swift
@@ -7,14 +7,85 @@
 //
 
 import Foundation
+import Combine
+import TangemSdk
 
 class DogecoinWalletManager: BitcoinWalletManager {
     override var minimalFee: Decimal { 0.01 }
     override var minimalFeePerByte: Decimal { 1 }
+    
+    override func getFee(amount: Amount, destination: String) -> AnyPublisher<[Amount], Error> {
+        // https://github.com/dogecoin/dogecoin/blob/master/doc/fee-recommendation.md
+        
+        let dogePerKiloByte: Decimal = 0.01
+        let bytesInKiloByte: Decimal = 1024
+
+        let satoshiPerByte = dogePerKiloByte * wallet.blockchain.decimalValue / bytesInKiloByte
+        let satoshiPerByteInteger = (satoshiPerByte.rounded(roundingMode: .up) as NSDecimalNumber).intValue
+        
+        let transactionSize: Int
+        do {
+            let changeScript = txBuilder.changeScript
+            let hashes = try txBuilder.bitcoinManager.buildForSign(
+                target: destination,
+                amount: amount.value,
+                feeRate: satoshiPerByteInteger,
+                changeScript: changeScript
+            )
+            
+            let signer = DummySigner()
+            let signatures = try hashes.map {
+                try signer.sign(hash: $0, walletPublicKey: signer.publicKey)
+            }
+            
+            let transactionData = try txBuilder.bitcoinManager.buildForSend(
+                target: destination,
+                amount: amount.value,
+                feeRate: satoshiPerByteInteger,
+                derSignatures: signatures,
+                changeScript: changeScript
+            )
+            transactionSize = transactionData.count
+        } catch {
+            print("Failed to calculate \(wallet.blockchain.displayName) fee", error)
+            return .anyFail(error: BlockchainSdkError.failedToLoadFee)
+        }
+        
+        let normalSatoshiFee = Decimal(integerLiteral: satoshiPerByteInteger * transactionSize) / wallet.blockchain.decimalValue
+
+        let minSatoshiFee = normalSatoshiFee * 0.8
+        let maxSatoshiFee = normalSatoshiFee * 1.2
+        
+        let feeAmounts = [minSatoshiFee, normalSatoshiFee, maxSatoshiFee]
+            .map {
+                Amount(with: wallet.blockchain, value: $0)
+            }
+        
+        return Just(feeAmounts)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
 }
 
 extension DogecoinWalletManager: DustRestrictable {
     var dustValue: Amount {
         .init(with: wallet.blockchain, value: minimalFee)
+    }
+}
+
+fileprivate class DummySigner {
+    let privateKey: Data
+    let publicKey: Wallet.PublicKey
+    
+    init() {
+        let keyPair = try! Secp256k1Utils().generateKeyPair()
+        let compressedPublicKey = try! Secp256k1Key(with: keyPair.publicKey).compress()
+        self.publicKey = Wallet.PublicKey(seedKey: compressedPublicKey, derivedKey: nil, derivationPath: nil)
+        self.privateKey = keyPair.privateKey
+    }
+    
+    func sign(hash: Data, walletPublicKey: Wallet.PublicKey) throws -> Data {
+        let signature = try Secp256k1Utils().sign(hash, with: privateKey)
+        return signature
     }
 }
