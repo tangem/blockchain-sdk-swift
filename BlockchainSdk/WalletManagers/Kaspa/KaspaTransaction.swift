e2a2728bd2aba8d277e65ea891bfaa6e2366c444
Andrey Chukavin <megadestroyer007@gmail.com>
2023-03-15 14:42:43 +0300

    IOS-3167 Initial kaspa transaction builder / kaspa transaction

---
+++ BlockchainSdk/WalletManagers/Kaspa/KaspaTransaction.swift
@@ -0,0 +1,9 @@
+//
+//  KaspaTransaction.swift
+//  BlockchainSdk
+//
+//  Created by Andrey Chukavin on 15.03.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
---
+++ BlockchainSdk/WalletManagers/Kaspa/KaspaTransactionBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  KaspaTransactionBuilder.swift
+//  BlockchainSdk
+//
+//  Created by Andrey Chukavin on 15.03.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+class KaspaTransactionBuilder {
+    var utxos: [BitcoinUnspentOutput] = []
+}
