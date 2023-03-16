3e4d0592a56a6a243347a283a7575a134c4176b7
Sergey Balashov <Balashov.152@gmail.com>
2023-03-16 12:37:02 +0100

    IOS-3185 return OptimismGasLoader

--- BlockchainSdk/WalletManagers/Optimism/OptimismWalletManager.swift
+++ BlockchainSdk/WalletManagers/Optimism/OptimismWalletManager.swift
@@ -91,3 +91,33 @@ private extension OptimismWalletManager {
             .eraseToAnyPublisher()
     }
 }
+
+//MARK: - OptimismGasLoader
+
+extension OptimismWalletManager: OptimismGasLoader {
+    func getLayer1GasPrice() -> AnyPublisher<BigUInt, Error> {
+        contractInteractor
+            .read(method: .l1BaseFee)
+            .tryMap { response in
+                if let bigUIntPrice = BigUInt("\(response)") {
+                    return bigUIntPrice
+                }
+                
+                throw BlockchainSdkError.failedToLoadFee
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getLayer1GasLimit(data: String) -> AnyPublisher<BigUInt, Error> {
+        contractInteractor
+            .read(method: .getL1GasUsed(data: data))
+            .tryMap { response in
+                if let bigUIntLimit = BigUInt("\(response)") {
+                    return bigUIntLimit
+                }
+                
+                throw BlockchainSdkError.failedToLoadFee
+            }
+            .eraseToAnyPublisher()
+    }
+}
