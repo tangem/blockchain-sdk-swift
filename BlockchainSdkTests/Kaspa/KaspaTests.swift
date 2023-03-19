2b03ec24118083b23ef16f54dd16398b9907114e
Andrey Chukavin <megadestroyer007@gmail.com>
2023-03-19 19:30:44 +0300

    IOS-3167 Added p2sh kaspa test

--- BlockchainSdkTests/Kaspa/KaspaTests.swift
+++ BlockchainSdkTests/Kaspa/KaspaTests.swift
@@ -15,10 +15,15 @@ import TangemSdk
 class KaspaTests: XCTestCase {
     private let blockchain = Blockchain.kaspa
     private let sizeTester = TransactionSizeTesterUtility()
+    private var txBuilder: KaspaTransactionBuilder!
     
-    func testBuildTransaction() {
-        let txBuilder = KaspaTransactionBuilder(blockchain: blockchain)
+    override func setUp() {
+        super.setUp()
         
+        self.txBuilder = KaspaTransactionBuilder(blockchain: blockchain)
+    }
+    
+    func testBuildSchnorrTransaction() {
         txBuilder.setUnspentOutputs([
             BlockchainSdk.BitcoinUnspentOutput(
                 transactionHash: "deb88e7dd734437c6232a636085ef917d1d13cc549fe14749765508b2782f2fb",
@@ -112,6 +117,82 @@ class KaspaTests: XCTestCase {
                 ),
                 BlockchainSdk.KaspaOutput(
                     amount: 519870000,
+                    scriptPublicKey: BlockchainSdk.KaspaScriptPublicKey(
+                        scriptPublicKey: "2103eb30400ce9d1deed12b84d4161a1fa922ef4185a155ef3ec208078b3807b126fab",
+                        version: 0
+                    )
+                )
+            ],
+            lockTime: 0,
+            subnetworkId: "0000000000000000000000000000000000000000"
+        )
+        
+        let encoder = JSONEncoder()
+        let encodedBuiltTransaction = try! encoder.encode(builtTransaction)
+        let encodedExpectedTransaction = try! encoder.encode(expectedTransaction)
+        XCTAssertEqual(encodedBuiltTransaction, encodedExpectedTransaction)
+    }
+    
+    func testP2SHTransaction() {
+        txBuilder.setUnspentOutputs([
+            BlockchainSdk.BitcoinUnspentOutput(
+                transactionHash: "ae96e819429e9da538e84cb213f62fbc8ad32e932d7c7f1fb9bd2fedf8fd7b4a",
+                outputIndex: 0,
+                amount: 500000000,
+                outputScript: "21034c88a1a83469ddf20d0c07e5c4a1e7b83734e721e60d642b94a53222c47c670dab"
+            ),
+        ])
+        
+        let walletPublicKey = "04EB30400CE9D1DEED12B84D4161A1FA922EF4185A155EF3EC208078B3807B126FA22C335081AAEBF161095C11C7D8BD550EF8882A3125B0EE9AE96DDDE1AE743F"
+        let sourceAddress = try! blockchain.getAddressService().makeAddress(from: Data(hex: walletPublicKey))
+        let destination = "kaspa:pqurku73qluhxrmvyj799yeyptpmsflpnc8pha80z6zjh6efwg3v2rrepjm5r"
+        
+        let transaction = Transaction(
+            amount: Amount(with: blockchain, value: 0.001),
+            fee: Amount(with: blockchain, value: 0.0001),
+            sourceAddress: sourceAddress,
+            destinationAddress: destination,
+            changeAddress: sourceAddress
+        )
+        
+        let (kaspaTransaction, hashes) = try! txBuilder.buildForSign(transaction)
+        
+        let expectedHashes = [
+            Data(hex: "C550515D34A091D7F3D2827286E7AEF685ECE9C0BBCCB4B08BC65F6EBD83E8F2"),
+        ]
+        XCTAssertEqual(hashes, expectedHashes)
+        
+        
+        let signatures = [
+            Data(hexString: "E2747D4E00C55D69FA0B8ADFAFD07F41144F888E322D377878E83F25FD2E258B2E918EF79E151337D7F3BD0798D66FDCE04B0704EB30400CE9D1DEED12B84D41"),
+        ]
+        
+        let builtTransaction = txBuilder.buildForSend(transaction: kaspaTransaction, signatures: signatures)
+        
+        let expectedTransaction = KaspaTransactionData(
+            version: 0,
+            inputs: [
+                BlockchainSdk.KaspaInput(
+                    previousOutpoint: BlockchainSdk.KaspaPreviousOutpoint(
+                        transactionId: "ae96e819429e9da538e84cb213f62fbc8ad32e932d7c7f1fb9bd2fedf8fd7b4a",
+                        index: 0
+                    ),
+                    signatureScript: "41e2747d4e00c55d69fa0b8adfafd07f41144f888e322d377878e83f25fd2e258b2e918ef79e151337d7f3bd0798d66fdce04b0704eb30400ce9d1deed12b84d4101",
+                    sequence: 0,
+                    sigOpCount: 1
+                ),
+            ],
+            outputs: [
+                BlockchainSdk.KaspaOutput(
+                    amount: 100000,
+                    scriptPublicKey: BlockchainSdk.KaspaScriptPublicKey(
+                        scriptPublicKey: "aa20383b73d107f9730f6c24bc5293240ac3b827e19e0e1bf4ef16852beb297222c587",
+                        version: 0
+                    )
+                    
+                ),
+                BlockchainSdk.KaspaOutput(
+                    amount: 499890000,
                     scriptPublicKey: BlockchainSdk.KaspaScriptPublicKey(
                         scriptPublicKey: "2103eb30400ce9d1deed12b84d4161a1fa922ef4185a155ef3ec208078b3807b126fab",
                         version: 0
