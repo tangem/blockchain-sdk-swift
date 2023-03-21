515240167467ef3fb4aa4e1572744176edf24229
Sergey Balashov <Balashov.152@gmail.com>
2023-03-21 14:38:57 +0100

    IOS-3185 return History

--- BlockchainSdk/WalletManagers/Optimism/OptimismSmartContractABI.swift
+++ BlockchainSdk/WalletManagers/Optimism/OptimismSmartContractABI.swift
@@ -2,8 +2,8 @@
 //  OptimismSmartContractABI.swift
 //  BlockchainSdk
 //
-//  Created by Sergey Balashov on 21.03.2023.
-//  Copyright © 2023 Tangem AG. All rights reserved.
+//  Created by Pavel Grechikhin on 06.10.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
 //
 
 import Foundation
