1e19c83f1aa4c3d84e3f3592fc63243aa38de37a
Andrey Chukavin <megadestroyer007@gmail.com>
2023-04-03 09:51:47 +0400

    IOS-3332 Added nownodes/getblock API URLs

--- BlockchainSdk/WalletManagers/Tron/TronNetwork.swift
+++ BlockchainSdk/WalletManagers/Tron/TronNetwork.swift
@@ -9,12 +9,18 @@
 import Foundation
 
 enum TronNetwork {
+    case nowNodes(apiKey: String)
+    case getBlock(apiKey: String)
     case mainnet
     case shasta
     case nile
     
     var url: URL {
         switch self {
+        case .nowNodes(let apiKey):
+            return URL(string: "https://trx.nownodes.io/\(apiKey)")!
+        case .getBlock(let apiKey):
+            return URL(string: "https://trx.getblock.io/mainnet/\(apiKey)")!
         case .mainnet:
             return URL(string: "https://api.trongrid.io")!
         case .shasta:
