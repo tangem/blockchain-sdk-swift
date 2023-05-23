5f93c3d4b6cd687852d61a55000caaca7b4ec114
Andrei Fedorov <m3g0byt3@gmail.com>
2023-05-23 16:56:57 +0300

    IOS-3654: Update `etc-network.info` ETC RPC with new URLs

--- BlockchainSdk/Common/Blockchain.swift
+++ BlockchainSdk/Common/Blockchain.swift
@@ -358,8 +358,10 @@ extension Blockchain {
                     URL(string: "https://etc.etcdesktop.com")!,
                     URL(string: "https://blockscout.com/etc/mainnet/api/eth-rpc")!,
                     URL(string: "https://etc.mytokenpocket.vip")!,
-                    URL(string: "https://besu.etc-network.info")!,
-                    URL(string: "https://geth.etc-network.info")!,
+                    URL(string: "https://besu-de.etc-network.info")!,
+                    URL(string: "https://besu-at.etc-network.info")!,
+                    URL(string: "https://geth-de.etc-network.info")!,
+                    URL(string: "https://geth-at.etc-network.info")!,
                 ]
             }
         case .ethereumPoW:
