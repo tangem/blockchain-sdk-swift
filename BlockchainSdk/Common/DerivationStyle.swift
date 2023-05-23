b49f0b3e0d06b86cb4ce0133e20ef4141f3e4b86
Sergey Balashov <Balashov.152@gmail.com>
2023-05-23 15:23:47 +0300

    IOS-3682 Update DerivationStyle.swift

--- BlockchainSdk/Common/DerivationStyle.swift
+++ BlockchainSdk/Common/DerivationStyle.swift
@@ -15,15 +15,15 @@ public enum DerivationStyle {
     @available(*, deprecated, message: "Will be removed after refactoring")
     case new
     
-    /// All have derivation according to `coinType`
+    /// All have derivation according to BIP44 `coinType`
     /// https://github.com/satoshilabs/slips/blob/master/slip-0044.md
     case v1
     
-    /// All EVM have identical derivation with `coinType == 60`
-    /// Other blockchains - according to `coinType`
+    /// `EVM-like` have identical derivation with `ethereumCoinType == 60`
+    /// Other blockchains - according to BIP44 `coinType`
     case v2
     
-    /// `EVM-like` blockchains have identical derivation with `coinType == 60`
+    /// `EVM-like` blockchains have identical derivation with `ethereumCoinType == 60`
     /// `Bitcoin-like` blockchains have different derivation related to `BIP`. For example `Legacy` and `SegWit`
     case v3
 }
