739273ec2aeb714b26c9ac62567098a2529c7eee
Andrey Chukavin <megadestroyer007@gmail.com>
2023-06-16 13:46:15 +0400

    IOS-3856 Added "sbility" fee for terra 1

--- BlockchainSdk/WalletManagers/Cosmos/CosmosChain.swift
+++ BlockchainSdk/WalletManagers/Cosmos/CosmosChain.swift
@@ -202,4 +202,16 @@ extension CosmosChain {
             return [:]
         }
     }
+    
+    func extraFee(for amount: Decimal) -> Decimal? {
+        switch self {
+        case .terraV1:
+            // Stability or "spread" fee
+            // https://classic-docs.terra.money/docs/learn/fees.html#spread-fee
+            let minimumSpreadFeePercentage: Decimal = 0.3//5
+            return amount * 0.01 * minimumSpreadFeePercentage
+        case .cosmos, .terraV2, .gaia:
+            return nil
+        }
+    }
 }
