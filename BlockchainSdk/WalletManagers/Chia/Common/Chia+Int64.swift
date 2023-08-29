c0a307c09fb7f7d2b80f0e3222665edb97468346
Skibin Alexander <skibinalexander@gmail.com>
2023-08-29 10:20:17 +0300

    IOS-4425 Fix review comments

--- BlockchainSdk/WalletManagers/Chia/Common/Chia+Int64.swift
+++ BlockchainSdk/WalletManagers/Chia/Common/Chia+Int64.swift
@@ -13,7 +13,8 @@ extension Int64 {
     /// Convert amount value for use in ClvmProgram for serialization
     /// - Returns: Binary data encoded
     var chiaEncoded: Data {
-        let unsafeDataValue = withUnsafeBytes(of: self) { Data($0) }.reversed().drop(while: { $0 == 0x00 })
+        let bigEndianData = withUnsafeBytes(of: self.bigEndian) { Data($0) }
+        let unsafeDataValue = bigEndianData.drop(while: { $0 == 0x00 })
         let serializeValue = BigInt(self).serialize()
         return (unsafeDataValue.first ?? 0x00) >= 0x80 ? serializeValue : serializeValue.dropFirst()
     }
