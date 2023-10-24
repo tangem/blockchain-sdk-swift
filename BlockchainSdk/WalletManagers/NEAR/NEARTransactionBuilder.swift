1f0de05ab4d4a6fca1906fa1bd8f2266fad821a4
Andrey Fedorov <m3g0byt3@gmail.com>
2023-10-24 14:11:03 +0300

    IOS-4071: Implement `build for sign` and `build for send` logic in transaction builder

--- BlockchainSdk/WalletManagers/NEAR/NEARTransactionBuilder.swift
+++ BlockchainSdk/WalletManagers/NEAR/NEARTransactionBuilder.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import WalletCore
+import BigInt
 
 final class NEARTransactionBuilder {
     private let blockchain: Blockchain
@@ -19,12 +20,109 @@ final class NEARTransactionBuilder {
     }
 
     func buildForSign(transaction: Transaction) throws -> Data {
-        // TODO: Andrey Fedorov - Add actual implementation (IOS-4071)
-        return Data()
+        guard let coinType = CoinType(blockchain) else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let input = try buildInput(transaction: transaction)
+        let txInputData = try input.serializedData()
+
+        guard !txInputData.isEmpty else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let preImageHashes = TransactionCompiler.preImageHashes(coinType: coinType, txInputData: txInputData)
+        let output = try TxCompilerPreSigningOutput(serializedData: preImageHashes)
+
+        guard output.error == .ok else {
+            throw WalletError.failedToBuildTx
+        }
+
+        return output.dataHash
     }
 
     func buildForSend(transaction: Transaction, signature: Data) throws -> Data {
-        // TODO: Andrey Fedorov - Add actual implementation (IOS-4071)
-        return Data()
+        guard 
+            let coinType = CoinType(blockchain),
+            let transactionParams = transaction.params as? NEARTransactionParams
+        else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let input = try buildInput(transaction: transaction)
+        let txInputData = try input.serializedData()
+
+        guard !txInputData.isEmpty else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let compiledTransaction = TransactionCompiler.compileWithSignatures(
+            coinType: coinType,
+            txInputData: txInputData,
+            signatures: signature.asDataVector(),
+            publicKeys: transactionParams.publicKey.blockchainKey.asDataVector()
+        )
+        let output = try NEARSigningOutput(serializedData: compiledTransaction)
+
+        guard output.error == .ok else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let serializedData = output.signedTransaction
+
+        guard !serializedData.isEmpty else {
+            throw WalletError.failedToBuildTx
+        }
+
+        return serializedData
+    }
+
+    private func buildInput(transaction: Transaction) throws -> NEARSigningInput {
+        guard let transactionParams = transaction.params as? NEARTransactionParams else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let deposit = try depositPayload(from: transaction.amount)
+
+        return NEARSigningInput.with { input in
+            input.publicKey = transactionParams.publicKey.blockchainKey
+            input.signerID = transaction.sourceAddress
+            input.receiverID = transaction.destinationAddress
+            input.blockHash = transactionParams.recentBlockHash.base58DecodedData
+            input.nonce = UInt64(transactionParams.currentNonce + 1)
+            input.actions = [
+                NEARAction.with { action in
+                    action.transfer = NEARTransfer.with { transfer in
+                        transfer.deposit = deposit
+                    }
+                }
+            ]
+        }
+    }
+
+    private func depositPayload(from amount: Amount) throws -> Data {
+        let decimalValue = amount.value * pow(Decimal(10), amount.decimals)
+        let bigUIntValue = try bigUIntValue(from: decimalValue)
+        let rawPayload = Data(bigUIntValue.serialize().reversed())
+
+        return rawPayload.trailingZeroPadding(toLength: 16)
+    }
+
+    private func bigUIntValue(from decimalValue: Decimal) throws -> BigUInt {
+        if decimalValue.isZero || decimalValue < .zero {
+            return .zero
+        }
+
+        if decimalValue >= .greatestFiniteMagnitude {
+            return BigUInt(2).power(256) - 1
+        }
+
+        let decimalNumber = decimalValue as NSDecimalNumber
+
+        guard let bigUIntValue = BigUInt(decimalNumber.stringValue) else {
+            throw WalletError.failedToBuildTx
+        }
+
+        return bigUIntValue
     }
 }
