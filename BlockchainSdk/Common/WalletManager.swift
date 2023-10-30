bff7e77ae8048d3758058ff032c065c8cc9db111
Andrey Fedorov <m3g0byt3@gmail.com>
2023-10-30 09:04:41 +0300

    IOS-4851: Introduce `AddressModifier` BSDK public interface

--- BlockchainSdk/Common/WalletManager.swift
+++ BlockchainSdk/Common/WalletManager.swift
@@ -105,6 +105,11 @@ public protocol WithdrawalValidator {
     func validate(_ transaction: Transaction) -> WithdrawalWarning?
 }
 
+@available(iOS 13.0, *)
+public protocol AddressModifier {
+    func modify(_ address: String) async throws -> String
+}
+
 public struct WithdrawalWarning {
     public let warningMessage: String
     public let reduceMessage: String
