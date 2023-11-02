a08ed60003029ba4cdd76d1d9fcd7c377a45c4b6
Andrey Fedorov <m3g0byt3@gmail.com>
2023-11-02 04:23:05 +0300

    IOS-5013: Update the order of ETC APIs

--- BlockchainSdk/Common/Blockchain.swift
+++ BlockchainSdk/Common/Blockchain.swift
@@ -386,9 +386,9 @@ extension Blockchain {
                 ]
             } else {
                 return [
+                    URL(string: "https://etc.etcdesktop.com")!,
                     URL(string: "https://etc.getblock.io/mainnet?api_key=\(getBlockApiKey)")!,
                     URL(string: "https://etc.rivet.link/etc")!,
-                    URL(string: "https://etc.etcdesktop.com")!,
                     URL(string: "https://blockscout.com/etc/mainnet/api/eth-rpc")!,
                     URL(string: "https://etc.mytokenpocket.vip")!,
                     URL(string: "https://besu-de.etc-network.info")!,
