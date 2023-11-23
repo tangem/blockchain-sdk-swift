2a1bc78bef14892a9b8bfb25ed3c2c6eec2b6659
Skibin Alexander <skibinalexander@gmail.com>
2023-11-23 14:53:17 +0300

    IOS-5167 Fix review comments

--- BlockchainSdk/WalletManagers/Decimal/DecimalAddressService.swift
+++ BlockchainSdk/WalletManagers/Decimal/DecimalAddressService.swift
@@ -14,8 +14,10 @@ struct DecimalAddressService {
     // MARK: - Private Properties
     
     private let isTestnet: Bool
+    private let ethereumAddressService = EthereumAddressService()
+    
     private let bech32 = Bech32(variant: .bech32)
-    private let utils = DecimalUtils()
+    private let utils = DecimalBlockchainAddressConverter()
     
     init(isTestnet: Bool) {
         self.isTestnet = isTestnet
@@ -26,43 +28,18 @@ struct DecimalAddressService {
 
 extension DecimalAddressService: AddressProvider {
     func makeAddress(for publicKey: Wallet.PublicKey, with addressType: AddressType) throws -> PlainAddress {
-        let ercAddressValue = try makeErcAddress(walletPublicKey: publicKey)
-        let dscAddressValue = try utils.convertErcAddressToDscAddress(addressHex: ercAddressValue)
+        var address = try ethereumAddressService.makeAddress(for: publicKey, with: addressType)
         
-        return .init(value: dscAddressValue, publicKey: publicKey, type: addressType)
-    }
-    
-    private func makeErcAddress(walletPublicKey: Wallet.PublicKey) throws -> String {
-        let walletPublicKey = try Secp256k1Key(with: walletPublicKey.blockchainKey).decompress()
-        //skip secp256k1 prefix
-        let keccak = walletPublicKey[1...].sha3(.keccak256)
-        let addressBytes = keccak[12...]
-        let address = addressBytes.hexString.addHexPrefix()
-        let checksumAddress = toChecksumAddress(address)!
-        return checksumAddress
-    }
-    
-    private func toChecksumAddress(_ address: String) -> String? {
-        let address = address.lowercased().removeHexPrefix()
-        guard let hash = address.data(using: .utf8)?.sha3(.keccak256).hexString.lowercased().removeHexPrefix() else {
-            return nil
+        // If need to convert address to decimal native type
+        if case .default = addressType {
+            address = try .init(
+                value: utils.convertErcAddressToDscAddress(addressHex: address.value),
+                publicKey: publicKey,
+                type: addressType
+            )
         }
         
-        var ret = "0x"
-        let hashChars = Array(hash)
-        let addressChars = Array(address)
-        for i in 0..<addressChars.count {
-            guard let intValue = Int(String(hashChars[i]), radix: 16) else {
-                return nil
-            }
-            
-            if intValue >= 8 {
-                ret.append(addressChars[i].uppercased())
-            } else {
-                ret.append(addressChars[i])
-            }
-        }
-        return ret
+        return .init(value: address.value, publicKey: publicKey, type: addressType)
     }
 }
 
@@ -73,35 +50,7 @@ extension DecimalAddressService: AddressValidator {
         guard let ercAddress = utils.convertDscAddressToErcAddress(addressHex: address) else {
             return false
         }
-
-        guard !ercAddress.isEmpty,
-              ercAddress.hasHexPrefix(),
-              ercAddress.count == 42
-        else {
-            return false
-        }
-
-
-        if let checksummed = toChecksumAddress(ercAddress),
-           checksummed == address {
-            return true
-        } else {
-            let cleanHex = address.stripHexPrefix()
-            if cleanHex.lowercased() != cleanHex && cleanHex.uppercased() != cleanHex {
-                return false
-            }
-        }
-
-        return true
-    }
-}
-
-// MARK: - Constants
-
-extension DecimalAddressService {
-    enum Constants {
-        static let addressPrefix = "d0"
-        static let legacyAddressPrefix = "dx"
-        static let erc55AddressPrefix = "0x"
+        
+        return ethereumAddressService.validate(ercAddress)
     }
 }
--- BlockchainSdk/WalletManagers/Decimal/DecimalWalletManager.swift
+++ BlockchainSdk/WalletManagers/Decimal/DecimalWalletManager.swift
@@ -12,25 +12,33 @@ import TangemSdk
 
 final class DecimalWalletManager: EthereumWalletManager {
     
+    override func getInfo(address: String, tokens: [Token], _ completion: @escaping (Result<Void, Error>) -> Void) {
+        let convertedDestinationAddress = convertAddressIfNeeded(destinationAddress: address)
+        super.getInfo(address: convertedDestinationAddress, tokens: tokens, completion)
+    }
+    
+    override func getFee(to: String, from: String, value: String?, data: Data?) -> AnyPublisher<[Fee], Error> {
+        let fromConvertedAddress = convertAddressIfNeeded(destinationAddress: from)
+        let toConvertedAddress = convertAddressIfNeeded(destinationAddress: to)
+        return super.getFee(to: toConvertedAddress, from: fromConvertedAddress, value: value, data: data)
+    }
+    
     override func sign(_ transaction: Transaction, signer: TransactionSigner) -> AnyPublisher<String, Error> {
-        let convertedDestinationAddress = convertAddress(destinationAddress: transaction.destinationAddress)
+        let sourceConvertedAddress = convertAddressIfNeeded(destinationAddress: transaction.sourceAddress)
+        let destinationConvertedAddress = convertAddressIfNeeded(destinationAddress: transaction.destinationAddress)
         
-        let copyTransaction = Transaction(
-            amount: transaction.amount,
-            fee: transaction.fee,
-            sourceAddress: transaction.sourceAddress,
-            destinationAddress: convertedDestinationAddress,
-            changeAddress: transaction.changeAddress,
-            params: transaction.params
-        )
+        let copyTransaction = transaction.then { trx in
+            trx.sourceAddress = sourceConvertedAddress
+            trx.destinationAddress = destinationConvertedAddress
+        }
         
         return super.sign(copyTransaction, signer: signer)
     }
     
     // MARK: - Private Implementation
 
-    private func convertAddress(destinationAddress: String) -> String {
-        return DecimalUtils().convertDscAddressToErcAddress(addressHex: destinationAddress) ?? destinationAddress
+    private func convertAddressIfNeeded(destinationAddress: String) -> String {
+        return DecimalBlockchainAddressConverter().convertDscAddressToErcAddress(addressHex: destinationAddress) ?? destinationAddress
     }
     
 }
