341b7924f2a8398f25e1c157ab3a6d36bf5fc41d
Alexander Osokin <tureckiy@gmail.com>
2023-12-05 13:27:03 +0000

    Merge pull request #465 from tangem/IOS-5167-add-decimal-blockchain
    Ios 5167 add decimal blockchain

---
+++ BlockchainSdk/WalletManagers/Decimal/DecimalTransactionBuilder.swift
@@ -0,0 +1,35 @@
+//
+//  DecimalTransactionBuilder.swift
+//  BlockchainSdk
+//
+//  Created by skibinalexander on 27.11.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+class DecimalTransactionBuilder: EthereumTransactionBuilder {
+    private let addressConverter = DecimalBlockchainAddressConverter()
+    
+    override func buildForSign(transaction: Transaction, nonce: Int) -> CompiledEthereumTransaction? {
+        do {
+            let sourceConvertedAddress = try convertAddressIfNeeded(destinationAddress: transaction.sourceAddress)
+            let destinationConvertedAddress = try convertAddressIfNeeded(destinationAddress: transaction.destinationAddress)
+            
+            let copyTransaction = transaction.then { trx in
+                trx.sourceAddress = sourceConvertedAddress
+                trx.destinationAddress = destinationConvertedAddress
+            }
+            
+            return super.buildForSign(transaction: copyTransaction, nonce: nonce)
+        } catch {
+            return nil
+        }
+    }
+    
+    // MARK: - Private Implementation
+    
+    private func convertAddressIfNeeded(destinationAddress: String) throws -> String {
+        try addressConverter.convertDecimalBlockchainAddressToDscAddress(addressHex: destinationAddress)
+    }
+}
