e02dedf0742b2d259738c731163b220e2a8e8a36
Andrey Fedorov <m3g0byt3@gmail.com>
2023-12-19 21:35:04 +0300

    IOS-5239: Implement network provider

--- BlockchainSdk/WalletManagers/VeChain/Network/VeChainNetworkProvider.swift
+++ BlockchainSdk/WalletManagers/VeChain/Network/VeChainNetworkProvider.swift
@@ -9,7 +9,6 @@
 import Foundation
 import Combine
 
-// TODO: Andrey Fedorov - Add actual implementation (IOS-5239)
 struct VeChainNetworkProvider: HostProvider {
     var host: String { baseURL.hostOrUnknown }
 
@@ -22,5 +21,52 @@ struct VeChainNetworkProvider: HostProvider {
     ) {
         self.baseURL = baseURL
         provider = NetworkProvider<VeChainTarget>(configuration: configuration)
+    }
+
+    func getAccountInfo(address: String) -> AnyPublisher<VeChainNetworkResult.AccountInfo, Error> {
+        return requestPublisher(for: .viewAccount(address: address))
+    }
+
+    func sendTransaction(
+        _ rawTransaction: String
+    ) -> AnyPublisher<VeChainNetworkResult.Transaction, Error> {
+        return requestPublisher(for: .sendTransaction(rawTransaction: rawTransaction))
+    }
+
+    func getTransactionStatus(
+        transactionHash: String,
+        includePending: Bool,
+        rawOutput: Bool
+    ) -> AnyPublisher<VeChainNetworkResult.TransactionStatus, Error> {
+        return requestPublisher(
+            for: .transactionStatus(transactionHash: transactionHash, includePending: includePending, rawOutput: rawOutput)
+        )
+    }
+
+    private func requestPublisher<T: Decodable>(
+        for target: VeChainTarget.Target
+    ) -> AnyPublisher<T, Swift.Error> {
+        return provider.requestPublisher(VeChainTarget(baseURL: baseURL, target: target))
+            .filterSuccessfulStatusCodes()
+            .map(T.self)
+            .mapError { moyaError -> Swift.Error in
+                switch moyaError {
+                case .jsonMapping,
+                        .objectMapping:
+                    return WalletError.failedToParseNetworkResponse
+                case .imageMapping,
+                        .stringMapping,
+                        .encodableMapping,
+                        .statusCode,
+                        .underlying,
+                        .requestMapping,
+                        .parameterEncoding:
+                    return moyaError
+                @unknown default:
+                    assertionFailure("Unknown error kind received: \(moyaError)")
+                    return moyaError
+                }
+            }
+            .eraseToAnyPublisher()
     }
 }
