48360faf9f44977cb123efc45d152fc63a5a415b
Andrey Fedorov <m3g0byt3@gmail.com>
2023-12-21 03:15:34 +0300

    IOS-5238: Add tx builder and params (WIP)

--- BlockchainSdk/WalletManagers/VeChain/VeChainTransactionBuilder.swift
+++ BlockchainSdk/WalletManagers/VeChain/VeChainTransactionBuilder.swift
@@ -7,15 +7,77 @@
 //
 
 import Foundation
+import TangemSdk
+import WalletCore
+import BigInt
 
 final class VeChainTransactionBuilder {
+    private var coinType: CoinType { .veChain }
+
     func buildForSign(transaction: Transaction) throws -> Data {
-        // TODO: Andrey Fedorov - Add actual implementation (IOS-5238)
-        return Data()
+        guard transaction.params is VeChainTransactionParams else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let input = try buildInput(transaction: transaction)
+        let txInputData = try input.serializedData()
+
+        guard !txInputData.isEmpty else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let preImageHashes = TransactionCompiler.preImageHashes(coinType: coinType, txInputData: txInputData)
+        let output = try TxCompilerPreSigningOutput(serializedData: preImageHashes)
+
+        guard output.error == .ok else {
+            throw WalletError.failedToBuildTx
+        }
+
+        return output.dataHash
     }
 
     func buildForSend(transaction: Transaction, signature: Data) throws -> Data {
-        // TODO: Andrey Fedorov - Add actual implementation (IOS-5238)
-        return Data()
+        guard let transactionParams = transaction.params as? VeChainTransactionParams else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let input = try buildInput(transaction: transaction)
+        let txInputData = try input.serializedData()
+
+        guard !txInputData.isEmpty else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let publicKey = try Secp256k1Key(with: transactionParams.publicKey.blockchainKey).decompress()
+
+        let compiledTransaction = TransactionCompiler.compileWithSignatures(
+            coinType: coinType,
+            txInputData: txInputData,
+            signatures: signature.asDataVector(),
+            publicKeys: publicKey.asDataVector()
+        )
+
+        let output = try VeChainSigningOutput(serializedData: compiledTransaction)
+
+        guard output.error == .ok else {
+            throw WalletError.failedToBuildTx
+        }
+
+        let serializedData = output.encoded
+
+        guard !serializedData.isEmpty else {
+            throw WalletError.failedToBuildTx
+        }
+
+        return serializedData
+    }
+
+    private func buildInput(transaction: Transaction) throws -> VeChainSigningInput {
+        guard let transactionParams = transaction.params as? VeChainTransactionParams else {
+            throw WalletError.failedToBuildTx
+        }
+
+        // TODO: Andrey Fedorov - Add actual implementation
+        return VeChainSigningInput()
     }
 }
