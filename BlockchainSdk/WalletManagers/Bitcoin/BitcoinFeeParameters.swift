5f699422818ab275ddb631ae7c958b9847693fa6
Sergey Balashov <Balashov.152@gmail.com>
2024-01-16 15:48:35 +0000

    IOS-5490 Add BitcoinFeeParameters

---
+++ BlockchainSdk/WalletManagers/Bitcoin/BitcoinFeeParameters.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinFeeParameters.swift
+//  BlockchainSdk
+//
+//  Created by Sergey Balashov on 16.01.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+public struct BitcoinFeeParameters: FeeParameters {
+    public let rate: Int
+
+    public init(rate: Int) {
+        self.rate = rate
+    }
+}
