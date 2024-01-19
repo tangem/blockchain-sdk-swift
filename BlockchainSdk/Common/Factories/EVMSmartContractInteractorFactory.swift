a20a9a8312c5a3387aa16bb66a1307d0844bceaa
Andrew Son <24321494+Andoran90@users.noreply.github.com>
2024-01-19 15:31:57 +0500

    IOS-5697: EVM Smart contract interactor

---
+++ BlockchainSdk/Common/Factories/EVMSmartContractInteractorFactory.swift
@@ -0,0 +1,51 @@
+//
+//  EVMSmartContractInteractorFactory.swift
+//  BlockchainSdk
+//
+//  Created by Andrew Son on 17/01/24.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+public struct EVMSmartContractInteractorFactory {
+    private let config: BlockchainSdkConfig
+
+    public init(config: BlockchainSdkConfig) {
+        self.config = config
+    }
+
+    public func makeInteractor(for blockchain: Blockchain) throws -> EVMSmartContractInteractor {
+        guard blockchain.isEvm else {
+            throw FactoryError.invalidBlockchain
+        }
+
+        let networkAssembly = NetworkProviderAssembly()
+        let networkService = EthereumNetworkService(
+            decimals: blockchain.decimalCount,
+            providers: networkAssembly.makeEthereumJsonRpcProviders(with: EVMNetworkProviderAssemblyInput(
+                blockchain: blockchain,
+                blockchainSdkConfig: config,
+                networkConfig: config.networkProviderConfiguration(for: blockchain)
+            )),
+            blockcypherProvider: nil,
+            abiEncoder: WalletCoreABIEncoder()
+        )
+
+        return networkService
+    }
+}
+
+public extension EVMSmartContractInteractorFactory {
+    enum FactoryError: Error {
+        case invalidBlockchain
+    }
+}
+
+private extension EVMSmartContractInteractorFactory {
+    struct EVMNetworkProviderAssemblyInput: NetworkProviderAssemblyInput {
+        var blockchain: Blockchain
+        var blockchainSdkConfig: BlockchainSdkConfig
+        var networkConfig: NetworkProviderConfiguration
+    }
+}
