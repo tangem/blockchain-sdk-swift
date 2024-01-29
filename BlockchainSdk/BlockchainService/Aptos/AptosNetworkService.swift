137f8e3efe53ab2e6f78bf0ae0431b4988aac010
Skibin Alexander <skibinalexander@gmail.com>
2024-01-29 11:00:36 +0300

    IOS-5745 Create aptos network service

---
+++ BlockchainSdk/BlockchainService/Aptos/AptosNetworkService.swift
@@ -0,0 +1,10 @@
+//
+//  AptosNetworkService.swift
+//  BlockchainSdk
+//
+//  Created by skibinalexander on 29.01.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
