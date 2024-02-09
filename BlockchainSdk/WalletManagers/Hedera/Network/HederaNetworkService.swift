9412fca8ed6893e59b93a2a4ee01409df5984233
Andrey Fedorov <m3g0byt3@gmail.com>
2024-02-09 11:47:50 +0300

    IOS-4556: Implement Hedera network service

--- BlockchainSdk/WalletManagers/Hedera/Network/HederaNetworkService.swift
+++ BlockchainSdk/WalletManagers/Hedera/Network/HederaNetworkService.swift
@@ -7,23 +7,89 @@
 //
 
 import Foundation
+import Combine
 
 final class HederaNetworkService {
     var currentProviderIndex: Int
 
-    private let blockchain: Blockchain
     private let consensusProvider: HederaConsensusNetworkProvider
     private let restProviders: [HederaRESTNetworkProvider]
 
     init(
-        blockchain: Blockchain,
         consensusProvider: HederaConsensusNetworkProvider,
         restProviders: [HederaRESTNetworkProvider]
     ) {
-        self.blockchain = blockchain
         self.consensusProvider = consensusProvider
         self.restProviders = restProviders
         currentProviderIndex = 0
+    }
+
+    func getAccountInfo(publicKey: Wallet.PublicKey) -> some Publisher<HederaAccountInfo, Error> {
+        return providerPublisher { provider in
+            return provider
+                .getAccounts(publicKey: publicKey.blockchainKey.hexString)
+                .tryMap { accounts in
+                    let account = accounts.accounts.first
+
+                    // Account ID is the only essential piece of information for a particular account,
+                    // account alias and account EVM address may not exist at all
+                    guard let accountId = account?.account else {
+                        throw WalletError.noAccount(message: "")    // TODO: Andrey Fedorov - Add an appropriate message or throw a different error?
+                    }
+                    
+                    return HederaAccountInfo(accountId: accountId, alias: account?.alias, evmAddress: account?.evmAddress)
+                }
+                .eraseToAnyPublisher()
+        }
+    }
+
+    func createAccount(request: HederaCreateAccountRequest) -> some Publisher<HederaAccountInfo, Error> {
+        // TODO: Andrey Fedorov - Add actual implementation (IOS-5888)
+        return Fail(error: WalletError.empty)
+    }
+
+    func getBalance(accountId: String) -> some Publisher<Decimal, Error> {
+        return consensusProvider.getBalance(accountId: accountId)
+    }
+
+    func getExchangeRate() -> some Publisher<HederaExchangeRate, Error> {
+        return providerPublisher { provider in
+            return provider
+                .getExchangeRates()
+                .map { exchangeRate in
+                    let current = Constants.centsPerDollar
+                    * Decimal(exchangeRate.current.hbarEquivalent)
+                    / Decimal(exchangeRate.current.centEquivalent)
+
+                    let next = Constants.centsPerDollar
+                    * Decimal(exchangeRate.next.hbarEquivalent)
+                    / Decimal(exchangeRate.next.centEquivalent)
+
+                    return HederaExchangeRate(currentHBARPerUSD: current, nextHBARPerUSD: next)
+                }
+                .eraseToAnyPublisher()
+        }
+    }
+
+    func send(transaction: HederaTransactionBuilder.CompiledTransaction) -> some Publisher<TransactionSendResult, Error> {
+        return consensusProvider.send(transaction: transaction)
+    }
+
+    func getTransactionInfo(transactionHash: String) -> some Publisher<HederaTransactionInfo, Error> {
+        return consensusProvider
+            .getTransactionInfo(transactionHash: transactionHash)
+            .map { transactionInfo in
+                let isSuccessful: Bool
+
+                switch transactionInfo.status {
+                case .ok, .success:
+                    isSuccessful = true
+                default:
+                    isSuccessful = false
+                }
+
+                return HederaTransactionInfo(isSuccessful: isSuccessful, hash: transactionInfo.hash)
+            }
     }
 }
 
@@ -31,4 +97,13 @@ final class HederaNetworkService {
 
 extension HederaNetworkService: MultiNetworkProvider {
     var providers: [HederaRESTNetworkProvider] { restProviders }
+}
+
+// MARK: - Constants
+
+private extension HederaNetworkService {
+    enum Constants {
+        static let centsPerDollar = Decimal(100)
+        static let hederaNetworkId = "hedera"
+    }
 }
