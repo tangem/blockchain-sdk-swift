82c7188f874cf22c9b260e76d8d21025a61d395f
Andrey Fedorov <m3g0byt3@gmail.com>
2024-02-09 03:08:48 +0300

    IOS-4556: Add actual REST API endpoints & targets

---
+++ BlockchainSdk/WalletManagers/Hedera/Network/HederaBaseURLConfig.swift
@@ -0,0 +1,14 @@
+//
+//  HederaBaseURLConfig.swift
+//  BlockchainSdk
+//
+//  Created by Andrey Fedorov on 08.02.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct HederaBaseURLConfig {
+    let helperNodeBaseURL: URL
+    let mirrorNodeBaseURL: URL
+}
--- BlockchainSdk/WalletManagers/Hedera/Network/HederaTarget.swift
+++ BlockchainSdk/WalletManagers/Hedera/Network/HederaTarget.swift
@@ -9,34 +9,83 @@
 import Foundation
 import Moya
 
-// TODO: Andrey Fedorov - Add actual implementation (IOS-4556)
 struct HederaTarget {
-    let baseURL: URL
+    let config: HederaBaseURLConfig
     let target: Target
 }
 
 // MARK: - Auxiliary types
 
 extension HederaTarget {
-    enum Target {}
+    enum Target {
+        case createAccount(networkId: String, publicKey: String, cardId: String, cardPublicKey: String)
+        case getAccounts(publicKey: String)
+        case getExchangeRate
+    }
 }
 
 // MARK: - TargetType protocol conformance
 
 extension HederaTarget: TargetType {
+    var baseURL: URL {
+        switch target {
+        case .createAccount:
+            return config.helperNodeBaseURL
+        case .getAccounts, .getExchangeRate:
+            return config.mirrorNodeBaseURL
+        }
+    }
+
     var path: String {
-        return ""
+        switch target {
+        case .createAccount:
+            return "network-account"
+        case .getAccounts:
+            return "accounts"
+        case .getExchangeRate:
+            return "exchangerate"
+        }
     }
 
     var method: Moya.Method {
-        return .get
+        switch target {
+        case .createAccount:
+            return .post
+        case .getAccounts, .getExchangeRate:
+            return .get
+        }
     }
 
     var task: Moya.Task {
-        return .requestPlain
+        switch target {
+        case .createAccount(let networkId, let publicKey, _, _):
+            let params = HederaNetworkParams.CreateAccount(networkId: networkId, publicWalletKey: publicKey)
+            return .requestJSONEncodable(params)
+        case .getAccounts(let publicKey):
+            let urlParameters: [String: Any] = [
+                "balance": false,
+                "account.publickey": publicKey,
+            ]
+            return .requestCompositeData(bodyData: Data(), urlParameters: urlParameters)
+        case .getExchangeRate:
+            return .requestPlain
+        }
     }
 
     var headers: [String: String]? {
-        return nil
+        var headers = [
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        ]
+
+        switch target {
+        case .createAccount(_, _, let cardId, let cardPublicKey):
+            headers["card_id"] = cardId
+            headers["card_public_key"] = cardPublicKey
+        case .getAccounts, .getExchangeRate:
+            break
+        }
+
+        return headers
     }
 }
