62e021936f5182e3cf5da52b1013c82bc9f908c4
Andrey Fedorov <m3g0byt3@gmail.com>
2024-02-10 01:37:00 +0300

    IOS-4557: Implement transaction builder

--- BlockchainSdk/WalletManagers/Hedera/HederaTransactionBuilder.swift
+++ BlockchainSdk/WalletManagers/Hedera/HederaTransactionBuilder.swift
@@ -7,16 +7,75 @@
 //
 
 import Foundation
+import Hedera
+import CryptoSwift
+import TangemSdk
 
 final class HederaTransactionBuilder {
-    func buildForSign(transaction: Transaction) throws -> Data {
-        // TODO: Andrey Fedorov - Add actual implementation (IOS-4557)
-        return Data()
+    private let wallet: Wallet
+
+    private lazy var client: Client = {
+        return wallet.blockchain.isTestnet ? Client.forTestnet() : Client.forMainnet()
+    }()
+
+    private var curve: EllipticCurve { wallet.blockchain.curve }
+
+    init(wallet: Wallet) {
+        self.wallet = wallet
     }
 
-    func buildForSend(transaction: Transaction, signature: Data) throws -> Data {
-        // TODO: Andrey Fedorov - Add actual implementation (IOS-4557)
-        return Data()
+    func buildForSign(transaction: Transaction) throws -> CompiledTransaction {
+        let amount = transaction.amount
+        let decimalValue = amount.value * pow(Decimal(10), amount.decimals)
+        let roundedValue = decimalValue.rounded(roundingMode: .down)
+        let transactionAmount = try Hbar(roundedValue, .tinybar)
+        let sourceAccountId = try AccountId(parsing: transaction.sourceAddress)
+        let destinationAccountId = try AccountId(parsing: transaction.destinationAddress)
+        let transactionId = TransactionId.generateFrom(sourceAccountId)
+        let transactionParams = transaction.params as? HederaTransactionParams
+
+        let transferTransaction = try TransferTransaction()
+            .hbarTransfer(sourceAccountId, transactionAmount.negated())
+            .hbarTransfer(destinationAccountId, transactionAmount)
+            .transactionId(transactionId)
+            .maxTransactionFee(Hbar.from(transaction.fee.amount.value))
+            .transactionMemo(transactionParams?.memo ?? "")
+            .freezeWith(client)
+
+        return CompiledTransaction(curve: curve, client: client, innerTransaction: transferTransaction)
+    }
+
+    func buildForSend(transaction: CompiledTransaction, signatures: [Data]) throws -> CompiledTransaction {
+        let publicKey = try getPublicKey()
+        let signatures = try normalizeSignatures(signatures)
+        transaction.addSignatures(publicKey, signatures)
+
+        return transaction
+    }
+
+    private func getPublicKey() throws -> Hedera.PublicKey {
+        switch curve {
+        case .ed25519, .ed25519_slip0010:
+            return try .fromBytesEd25519(wallet.publicKey.blockchainKey)
+        case .secp256k1:
+            let ecdsaKey = try Secp256k1Key(with: wallet.publicKey.blockchainKey).compress()
+            return try .fromBytesEcdsa(ecdsaKey)
+        default:
+            throw HederaError.unsupportedCurve(curveName: curve.rawValue)
+        }
+    }
+
+    private func normalizeSignatures(_ signatures: [Data]) throws -> [Data] {
+        switch curve {
+        case .ed25519, .ed25519_slip0010:
+            return signatures
+        case .secp256k1:
+            return try signatures
+                .map { try Secp256k1Signature(with: $0) }
+                .map { try $0.normalize() }
+        default:
+            throw HederaError.unsupportedCurve(curveName: curve.rawValue)
+        }
     }
 }
 
@@ -24,14 +83,41 @@ final class HederaTransactionBuilder {
 
 extension HederaTransactionBuilder {
     struct CompiledTransaction {
-        var hashesToSign: [Data] {
-            // TODO: Andrey Fedorov - Add actual implementation (IOS-4557)
-            return []
+        private let curve: EllipticCurve
+        private let client: Hedera.Client
+        private let innerTransaction: Hedera.Transaction
+
+        fileprivate init(
+            curve: EllipticCurve,
+            client: Hedera.Client,
+            innerTransaction: Hedera.Transaction
+        ) {
+            self.curve = curve
+            self.client = client
+            self.innerTransaction = innerTransaction
+        }
+
+        func hashesToSign() throws -> [Data] {
+            let hashes = try innerTransaction.signedTransactionsData()
+            switch curve {
+            case .ed25519, .ed25519_slip0010:
+                return hashes
+            case .secp256k1:
+                return hashes.map { $0.sha3(.keccak256) }
+            default:
+                throw HederaError.unsupportedCurve(curveName: curve.rawValue)
+            }
+        }
+
+        func addSignatures(_ publicKey: PublicKey, _ signatures: [Data]) {
+            innerTransaction.addSignatures(publicKey, signatures)
         }
 
         func sendAndGetHash() async throws -> String {
-            // TODO: Andrey Fedorov - Add actual implementation (IOS-4557)
-            return ""
+            return try await innerTransaction
+                .execute(client)
+                .transactionId
+                .toString()
         }
     }
 }
