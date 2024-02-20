e95664814ff01fefc9b39ca6a832c8a477cb4464
Andrey Fedorov <m3g0byt3@gmail.com>
2024-02-20 12:59:44 +0300

    IOS-4562: Fix merge issues

--- BlockchainSdkTests/Extensions/TWPublicKeyType+.swift
+++ BlockchainSdkTests/Extensions/TWPublicKeyType+.swift
@@ -92,25 +92,3 @@ extension PublicKeyType {
         }
     }
 }
-
-private extension NSError {
-    static func makeUnsupportedCurveError(for blockchain: BlockchainSdk.Blockchain) -> Error {
-        return NSError(
-            domain: "com.tangem.BlockchainSDKTests",
-            code: -1,
-            userInfo: [
-                NSLocalizedDescriptionKey: "Unsupported curve \"\(blockchain.curve)\" for blockchain \"\(blockchain)\"",
-            ]
-        )
-    }
-
-    static func makeUnsupportedBlockchainError(for blockchain: BlockchainSdk.Blockchain) -> Error {
-        return NSError(
-            domain: "com.tangem.BlockchainSDKTests",
-            code: -2,
-            userInfo: [
-                NSLocalizedDescriptionKey: "Unsupported blockchain \"\(blockchain)\"",
-            ]
-        )
-    }
-}
