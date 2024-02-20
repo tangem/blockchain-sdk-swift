85c7fadc68255ce7124c8b47e2efcc61ec89c142
Skibin Alexander <skibinalexander@gmail.com>
2024-02-20 20:15:42 +0300

    IOS-6029 Remove minimal balance subscription

--- BlockchainSdk/WalletManagers/Algorand/AlgorandWalletManager.swift
+++ BlockchainSdk/WalletManagers/Algorand/AlgorandWalletManager.swift
@@ -69,6 +69,10 @@ final class AlgorandWalletManager: BaseManager {
 
 private extension AlgorandWalletManager {
     func validateExistentialDeposit(with accountModel: AlgorandAccountModel) throws {
+        /*
+         Every account on Algorand must have a minimum balance of 100,000 microAlgos. If ever a transaction is sent that would result in a balance lower than the minimum, the transaction will fail. The minimum balance increases with each asset holding the account has (whether the asset was created or owned by the account) and with each application the account created or opted in. Destroying a created asset, opting out/closing out an owned asset, destroying a created app, or opting out an opted in app decreases accordingly the minimum balance.
+         */
+        
         if accountModel.coinValue < accountModel.reserveValue {
             let error = makeNoAccountError(using: accountModel)
             throw error
