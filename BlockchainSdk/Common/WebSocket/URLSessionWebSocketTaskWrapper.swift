a51510185d6e7d06167e1bf14172cae90b2aecab
Sergey Balashov <Balashov.152@gmail.com>
2024-03-11 18:10:40 +0000

    IOS-6262 WebSocketConnection
    IOS-6078 Try async/await
    
    Try actor
    
    Try actor 2
    
    Try actor 3
    
    Working actor 1 (with leak)
    
    Working actor 2 (renames)
    
    Final async/await
    
    Grooming code
    
    IOS-6078 Try async/await
    
    Try actor
    
    Try actor 2
    
    Try actor 3
    
    Working actor 1 (with leak)
    
    Working actor 2 (renames)
    
    Final async/await
    
    Grooming code
    
    IOS-6262 Revert Electrum changes

---
+++ BlockchainSdk/Common/WebSocket/URLSessionWebSocketTaskWrapper.swift
@@ -0,0 +1,112 @@
+//
+//  URLSessionWebSocketTaskWrapper.swift
+//  BlockchainSdk
+//
+//  Created by Sergey Balashov on 12.03.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+/// A wrapper to work with `URLSessionWebSocketTask` through async/await method
+class URLSessionWebSocketTaskWrapper {
+    private let url: URL
+    private let delegateQueue: OperationQueue
+    
+    private var webSocketTaskDidOpen: CheckedContinuation<Void, Never>?
+    private var webSocketTaskDidClose: CheckedContinuation<URLSessionWebSocketTask.CloseCode, Never>?
+    private var session: URLSession?
+    private var sessionWebSocketTask: URLSessionWebSocketTask?
+    
+    init(
+        url: URL,
+        delegateQueue: OperationQueue = .init()
+    ) {
+        self.url = url
+        self.delegateQueue = delegateQueue
+    }
+    
+    deinit {
+        // We have to disconnect here that release all objects
+        disconnect()
+    }
+    
+    func sendPing() async throws {
+        guard let sessionWebSocketTask else {
+            throw WebSocketTaskError.webSocketNotFound
+        }
+
+        return try await withCheckedThrowingContinuation { continuation in
+            sessionWebSocketTask.sendPing { error in
+                switch error {
+                case .none:
+                    continuation.resume()
+                case .some(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    func send(message: URLSessionWebSocketTask.Message) async throws {
+        guard let sessionWebSocketTask else {
+            throw WebSocketTaskError.webSocketNotFound
+        }
+
+        try await sessionWebSocketTask.send(message)
+    }
+    
+    func receive() async throws -> URLSessionWebSocketTask.Message {
+        guard let sessionWebSocketTask else {
+            throw WebSocketTaskError.webSocketNotFound
+        }
+
+        return try await sessionWebSocketTask.receive()
+    }
+    
+    func connect() async {
+        // The delegate will be kept by URLSession
+        let delegate = URLSessionWebSocketDelegateWrapper(
+            webSocketTaskDidOpen: { [weak self] task in
+                self?.webSocketTaskDidOpen?.resume()
+            },
+            webSocketTaskDidClose: { [weak self] task, closeCode in
+                self?.webSocketTaskDidClose?.resume(returning: closeCode)
+            }
+        )
+        
+        session = URLSession(configuration: .default, delegate: delegate, delegateQueue: delegateQueue)
+        sessionWebSocketTask = session?.webSocketTask(with: url)
+        sessionWebSocketTask?.resume()
+
+        return await withCheckedContinuation { [weak self] continuation in
+            self?.webSocketTaskDidOpen = continuation
+        }
+    }
+    
+    func disconnect() {
+        sessionWebSocketTask?.cancel(with: .goingAway, reason: nil)
+        // Important for release of the session's delegate
+        session?.invalidateAndCancel()
+    }
+    
+    func disconnect() async -> URLSessionWebSocketTask.CloseCode {
+        sessionWebSocketTask?.cancel(with: .goingAway, reason: nil)
+        
+        return await withCheckedContinuation { [weak self] continuation in
+            self?.webSocketTaskDidClose = continuation
+        }
+    }
+}
+
+extension URLSessionWebSocketTaskWrapper: CustomStringConvertible {
+    var description: String {
+        objectDescription(self)
+    }
+}
+
+// MARK: - Error
+
+enum WebSocketTaskError: Error {
+    case webSocketNotFound
+}
---
+++ BlockchainSdk/Common/WebSocket/WebSocketConnection.swift
@@ -0,0 +1,251 @@
+//
+//  WebSocketConnection.swift
+//  BlockchainSdk
+//
+//  Created by Sergey Balashov on 11.03.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+actor WebSocketConnection {
+    private let url: URL
+    private let ping: Ping
+    private let timeout: TimeInterval
+    
+    private var _sessionWebSocketTask: Task<URLSessionWebSocketTaskWrapper, Never>?
+    private var pingTask: Task<Void, Error>?
+    private var timeoutTask: Task<Void, Error>?
+
+    /// - Parameters:
+    ///   - url: A `wss` URL
+    ///   - ping: The value that will be sent after a certain interval in seconds
+    ///   - timeout: The value in seconds through which the connection will be terminated, if there are no new `send` calls
+    init(url: URL, ping: Ping, timeout: TimeInterval) {
+        self.url = url
+        self.ping = ping
+        self.timeout = timeout
+        
+        log("init")
+    }
+    
+    public func send(_ message: URLSessionWebSocketTask.Message) async throws {
+        let webSocketTask = await setupWebSocketTask()
+        log("Send: \(message)")
+
+        // Send a message
+        try await webSocketTask.send(message: message)
+        startPingTask()
+
+        // Restart the disconnect timer
+        startTimeoutTask()
+    }
+
+    public func receive() async throws -> Data {
+        guard let webSocket = await _sessionWebSocketTask?.value else {
+            throw WebSocketConnectionError.webSocketNotFound
+        }
+        
+        // Get a message from the last response
+        let response = try await webSocket.receive()
+        log("Receive: \(response)")
+        
+        let data = try mapToData(from: response)
+        return data
+    }
+}
+
+// MARK: - Private
+
+private extension WebSocketConnection {
+    func startPingTask() {
+        pingTask?.cancel()
+        pingTask = Task { [weak self] in
+            guard let self else { return }
+
+            try await Task.sleep(nanoseconds: UInt64(ping.interval) * NSEC_PER_SEC)
+            
+            try Task.checkCancellation()
+            
+            try await ping()
+        }
+    }
+    
+    func startTimeoutTask() {
+        timeoutTask?.cancel()
+        timeoutTask = Task { [weak self] in
+            guard let self else { return }
+
+            try await Task.sleep(nanoseconds: UInt64(timeout) * NSEC_PER_SEC)
+            
+            try Task.checkCancellation()
+            
+            await disconnect()
+        }
+    }
+    
+    func ping() async throws {
+        guard let webSocket = await _sessionWebSocketTask?.value else {
+            throw WebSocketConnectionError.webSocketNotFound
+        }
+        
+        switch ping {
+        case .message(_, let message):
+            log("Send ping: \(message)")
+            try await webSocket.send(message: message)
+
+        case .plain:
+            log("Send plain ping")
+            try await webSocket.sendPing()
+        }
+        
+        startPingTask()
+    }
+    
+    func setupWebSocketTask() async -> URLSessionWebSocketTaskWrapper {
+        if let _sessionWebSocketTask {
+            let socket = await _sessionWebSocketTask.value
+            log("Return existed URLSessionWebSocketTaskWrapper \(socket)")
+            return socket
+        }
+
+        let connectingTask = Task {
+            let socket = URLSessionWebSocketTaskWrapper(url: url)
+            
+            log("\(socket) start connect")
+            await socket.connect()
+            log("\(socket) did open")
+
+            return socket
+        }
+        
+        _sessionWebSocketTask = connectingTask
+        return await connectingTask.value
+    }
+    
+    func mapToData(from message: URLSessionWebSocketTask.Message) throws -> Data {
+        switch message {
+        case .data(let data):
+            return data
+            
+        case .string(let string):
+            guard let data = string.data(using: .utf8) else {
+                throw WebSocketConnectionError.invalidResponse
+            }
+
+            return data
+            
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func disconnect() {
+        pingTask?.cancel()
+        timeoutTask?.cancel()
+        _sessionWebSocketTask?.cancel()
+        _sessionWebSocketTask = nil
+    }
+    
+    nonisolated func log(_ args: Any) {
+        print("\(self) [\(args)]")
+   }
+}
+
+extension WebSocketConnection: CustomStringConvertible {
+    nonisolated var description: String {
+        objectDescription(self)
+    }
+}
+
+// MARK: - Model
+
+extension WebSocketConnection {
+    enum Ping {
+        case plain(interval: TimeInterval)
+        case message(interval: TimeInterval, message: URLSessionWebSocketTask.Message)
+        
+        var interval: TimeInterval {
+            switch self {
+            case .plain(let interval):
+                return interval
+            case .message(let interval, _):
+                return interval
+            }
+        }
+    }
+}
+
+// MARK: - Error
+
+enum WebSocketConnectionError: Error {
+    case webSocketNotFound
+    case invalidResponse
+}
+
+// MARK: - URLSessionTask.State + CustomStringConvertible
+
+extension URLSessionTask.State: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .running:
+            return "URLSessionTask.State.running"
+        case .suspended:
+            return "URLSessionTask.State.suspended"
+        case .canceling:
+            return "URLSessionTask.State.canceling"
+        case .completed:
+            return "URLSessionTask.State.completed"
+        @unknown default:
+            return "URLSessionTask.State.@unknowndefault"
+        }
+    }
+}
+
+extension URLSessionWebSocketTask.Message: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .data(let data):
+            return "URLSessionWebSocketTask.Message.data: \(data)"
+        case .string(let string):
+            return "URLSessionWebSocketTask.Message.string: \(string)"
+        @unknown default:
+            return "URLSessionWebSocketTask.Message.@unknowndefault"
+        }
+    }
+}
+
+extension URLSessionWebSocketTask.CloseCode: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .invalid:
+            return "URLSessionWebSocketTask.CloseCode.invalid"
+        case .normalClosure:
+            return "URLSessionWebSocketTask.CloseCode.normalClosure"
+        case .goingAway:
+            return "URLSessionWebSocketTask.CloseCode.goingAway"
+        case .protocolError:
+            return "URLSessionWebSocketTask.CloseCode.protocolError"
+        case .unsupportedData:
+            return "URLSessionWebSocketTask.CloseCode.unsupportedData"
+        case .noStatusReceived:
+            return "URLSessionWebSocketTask.CloseCode.noStatusReceived"
+        case .abnormalClosure:
+            return "URLSessionWebSocketTask.CloseCode.abnormalClosure"
+        case .invalidFramePayloadData:
+            return "URLSessionWebSocketTask.CloseCode.invalidFramePayloadData"
+        case .policyViolation:
+            return "URLSessionWebSocketTask.CloseCode.policyViolation"
+        case .messageTooBig:
+            return "URLSessionWebSocketTask.CloseCode.messageTooBig"
+        case .mandatoryExtensionMissing:
+            return "URLSessionWebSocketTask.CloseCode.mandatoryExtensionMissing"
+        case .internalServerError:
+            return "URLSessionWebSocketTask.CloseCode.internalServerError"
+        case .tlsHandshakeFailure:
+            return "URLSessionWebSocketTask.CloseCode.tlsHandshakeFailure"
+        @unknown default:
+            return "URLSessionWebSocketTask.CloseCode.@unknowndefault"
+        }
+    }
+}
