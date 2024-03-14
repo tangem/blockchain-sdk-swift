f945772fe86ed658291b49ebabca83e369521c77
Andrey Chukavin <megadestroyer007@gmail.com>
2024-03-14 15:21:52 +0800

    IOS-6235 Added SolanaFeeParameters

---
+++ BlockchainSdk/Blockchains/Solana/SolanaFeeParameters.swift
@@ -0,0 +1,19 @@
+//
+//  SolanaFeeParameters.swift
+//  BlockchainSdk
+//
+//  Created by Andrey Chukavin on 14.03.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+public struct SolanaFeeParameters: FeeParameters {
+    public let computeUnitLimit: UInt32
+    public let computeUnitPrice: UInt64
+
+    public init(computeUnitLimit: UInt32, computeUnitPrice: UInt64) {
+        self.computeUnitLimit = computeUnitLimit
+        self.computeUnitPrice = computeUnitPrice
+    }
+}
