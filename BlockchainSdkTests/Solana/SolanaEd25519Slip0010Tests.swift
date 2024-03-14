e27e932b0849676d98368cf7878b05111cdce4ca
Andrey Chukavin <megadestroyer007@gmail.com>
2024-03-14 22:13:58 +0800

    IOS-6235 Passing fee parameters in tests

--- BlockchainSdkTests/Solana/SolanaEd25519Slip0010Tests.swift
+++ BlockchainSdkTests/Solana/SolanaEd25519Slip0010Tests.swift
@@ -61,6 +61,7 @@ final class SolanaEd25519Slip0010Tests: XCTestCase {
     private let walletPubKey = Data(hex: "B148CC30B144E8F214AE5754C753C40A9BF2A3359DB4246E03C6A2F61A82C282")
     private let address = "Cw3YcfqzRSa7xT7ecpR5E4FKDQU6aaxz5cWje366CZbf"
     private let blockchain = Blockchain.solana(curve: .ed25519_slip0010, testnet: true)
+    private let feeParameters = SolanaFeeParameters(computeUnitLimit: nil, computeUnitPrice: nil, accountCreationFee: 0)
     
     private let coinSigner = CoinSigner()
     private let tokenSigner = TokenSigner()
@@ -83,7 +84,7 @@ final class SolanaEd25519Slip0010Tests: XCTestCase {
     func testCoinTransactionSize() {
         let transaction = Transaction(
             amount: .zeroCoin(for: blockchain),
-            fee: Fee(.zeroCoin(for: blockchain)),
+            fee: Fee(.zeroCoin(for: blockchain), parameters: feeParameters),
             sourceAddress: manager.wallet.address,
             destinationAddress: "BmAzxn8WLYU3gEw79ATUdSUkMT53MeS5LjapBQB8gTPJ",
             changeAddress: manager.wallet.address,
@@ -105,7 +106,7 @@ final class SolanaEd25519Slip0010Tests: XCTestCase {
         )
         let transaction = Transaction(
             amount: Amount(with: blockchain, type: type, value: 0),
-            fee: Fee(Amount(with: blockchain, type: type, value: 0)),
+            fee: Fee(Amount(with: blockchain, type: type, value: 0), parameters: feeParameters),
             sourceAddress: manager.wallet.address,
             destinationAddress: "BmAzxn8WLYU3gEw79ATUdSUkMT53MeS5LjapBQB8gTPJ",
             changeAddress: manager.wallet.address
