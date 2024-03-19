63aaf40b3c17ef1873e0f9a5ab4ccc3cfcb6ffea
Sergey Balashov <Balashov.152@gmail.com>
2024-03-19 17:07:07 +0000

    IOS-6319 Add Nexa's foundation

---
+++ BlockchainSdk/Blockchains/Nexa/NexaAddressService.swift
@@ -0,0 +1,13 @@
+//
+//  NexaAddressService.swift
+//  BlockchainSdk
+//
+//  Created by Sergey Balashov on 19.03.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+class NexaAddressService {
+    
+}
---
+++ BlockchainSdk/Blockchains/Nexa/NexaTransactionBuilder.swift
@@ -0,0 +1,14 @@
+//
+//  NexaTransactionBuilder.swift
+//  BlockchainSdk
+//
+//  Created by Sergey Balashov on 19.03.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+// Decoder: https://explorer.nexa.org/decoder
+class NexaTransactionBuilder {
+    
+}
