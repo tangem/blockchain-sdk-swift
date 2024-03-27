c14f64c832e8664b5b991c3e60f8169deb5e9fac
Andrey Fedorov <afedorov@tangem.com>
2024-03-27 08:07:13 +0300

    IOS-6263: Implement retries for API requests
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- BlockchainSdk/Blockchains/Polkadot/AccountHealthCheck/Network/SubscanPolkadotAccountHealthNetworkService.swift
+++ BlockchainSdk/Blockchains/Polkadot/AccountHealthCheck/Network/SubscanPolkadotAccountHealthNetworkService.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import Moya
+import enum Moya.MoyaError
 
 public final class SubscanPolkadotAccountHealthNetworkService {
     private let provider = NetworkProvider<SubscanAPITarget>()
@@ -31,59 +31,120 @@ public final class SubscanPolkadotAccountHealthNetworkService {
     }
 
     public func getAccountHealthInfo(account: String) async throws -> PolkadotAccountHealthInfo {
-        let result = try await provider
-            .asyncRequest(
-                for: .init(
-                    isTestnet: isTestnet,
-                    encoder: encoder,
-                    target: .getAccountInfo(address: account)
-                )
-            )
-            .filterSuccessfulStatusAndRedirectCodes()
-            .tryMap(output: SubscanAPIResult.AccountInfo.self, failure: SubscanAPIResult.Error.self, using: decoder)
-            .data
-            .account
+        let result = try await perform(
+            request: .init(
+                isTestnet: isTestnet,
+                encoder: encoder,
+                target: .getAccountInfo(address: account)
+            ),
+            output: SubscanAPIResult.AccountInfo.self,
+            failure: SubscanAPIResult.Error.self,
+            retryAttempt: 0
+        )
+        .data
+        .account
 
         return PolkadotAccountHealthInfo(extrinsicCount: result.countExtrinsic, nonceCount: result.nonce)
     }
     
     public func getTransactionsList(account: String, afterId: Int) async throws -> [PolkadotTransaction] {
-        let result = try await provider
-            .asyncRequest(
-                for: .init(
-                    isTestnet: isTestnet,
-                    encoder: encoder,
-                    target: .getExtrinsicsList(
-                        address: account,
-                        afterId: afterId,
-                        page: Constants.startPage,
-                        limit: pageSize
-                    )
+        let result = try await perform(
+            request: .init(
+                isTestnet: isTestnet,
+                encoder: encoder,
+                target: .getExtrinsicsList(
+                    address: account,
+                    afterId: afterId,
+                    page: Constants.startPage,
+                    limit: pageSize
                 )
-            )
-            .filterSuccessfulStatusAndRedirectCodes()
-            .tryMap(output: SubscanAPIResult.ExtrinsicsList.self, failure: SubscanAPIResult.Error.self, using: decoder)
-            .data
-            .extrinsics
+            ),
+            output: SubscanAPIResult.ExtrinsicsList.self,
+            failure: SubscanAPIResult.Error.self,
+            retryAttempt: 0
+        )
+        .data
+        .extrinsics
 
         return result?.map { PolkadotTransaction(id: $0.id, hash: $0.extrinsicHash) } ?? []
     }
     
     public func getTransactionDetails(hash: String) async throws -> PolkadotTransactionDetails {
-        let result = try await provider
-            .asyncRequest(
-                for: .init(
-                    isTestnet: isTestnet,
-                    encoder: encoder,
-                    target: .getExtrinsicInfo(hash: hash)
-                )
-            )
-            .filterSuccessfulStatusAndRedirectCodes()
-            .tryMap(output: SubscanAPIResult.ExtrinsicInfo.self, failure: SubscanAPIResult.Error.self, using: decoder)
-            .data
-            .lifetime
+        let result = try await perform(
+            request: .init(
+                isTestnet: isTestnet,
+                encoder: encoder,
+                target: .getExtrinsicInfo(hash: hash)
+            ),
+            output: SubscanAPIResult.ExtrinsicInfo.self,
+            failure: SubscanAPIResult.Error.self,
+            retryAttempt: 0
+        )
+        .data
+        .lifetime
 
         return PolkadotTransactionDetails(birth: result?.birth, death: result?.death)
+    }
+
+    private func perform<Output, Failure>(
+        request: SubscanAPITarget,
+        output: Output.Type,
+        failure: Failure.Type,
+        retryAttempt: Int
+    ) async throws -> Output where Output: Decodable, Failure: Decodable, Failure: Error {
+        do {
+            return try await provider
+                .asyncRequest(for: request)
+                .filterSuccessfulStatusAndRedirectCodes()
+                .tryMap(output: output, failure: failure, using: decoder)
+        } catch {
+            guard
+                retryAttempt < Constants.maxRetryCount,
+                shouldRetry(error)
+            else {
+                throw error
+            }
+
+            let nextRetryAttempt = retryAttempt + 1
+            try await Task.sleep(nanoseconds: makeRetryInterval(retryAttempt: nextRetryAttempt))
+
+            return await try perform(request: request, output: output, failure: failure, retryAttempt: nextRetryAttempt)
+        }
+    }
+
+    /// - Warning: allows retries for ANY errors except mapping and cancellation errors, use with caution.
+    private func shouldRetry(_ error: Error) -> Bool {
+        if error is DecodingError {
+            return false
+        }
+
+        if error.asMoyaError?.isMappingError == true {
+            return false
+        }
+
+        if error is CancellationError {
+            return false
+        }
+
+        let urlErrorCode = (error as? URLError)?.code
+        let afErrorCode = (error.asAFError?.underlyingError as? URLError)?.code
+        let moyaErrorCode = (error.asMoyaError?.underlyingError as? URLError)?.code
+        let errorCode = urlErrorCode ?? afErrorCode ?? moyaErrorCode
+
+        if let errorCode, errorCode == .cancelled {
+            return false
+        }
+
+        return true
+    }
+
+    /// Provides exponential backoff with random jitter using standard formula `base * pow(2, retryAttempt) ± jitter`.
+    /// - Returns: Retry interval in nanoseconds.
+    private func makeRetryInterval(retryAttempt: Int) -> UInt64 {
+        let retryJitter: TimeInterval = .random(in: Constants.retryJitterMinValue...Constants.retryJitterMaxValue)
+        let retryIntervalSeconds = Constants.retryBaseValue * pow(2.0, TimeInterval(retryAttempt)) + retryJitter
+
+        return UInt64(retryIntervalSeconds * TimeInterval(NSEC_PER_SEC))
     }
 }
 
@@ -93,5 +154,55 @@ private extension SubscanPolkadotAccountHealthNetworkService {
     enum Constants {
         // - Note: Subscan API has zero-based indexing
         static let startPage = 0
+        static let maxRetryCount = 3
+        static var retryBaseValue: TimeInterval { 1.0 }
+        static var retryJitterMinValue: TimeInterval { -0.5 }
+        static var retryJitterMaxValue: TimeInterval { 0.5 }
+    }
+}
+
+// MARK: - Convenience extensions
+
+private extension MoyaError {
+    // Just a copy-paste from MoyaError.swift (it has `internal` access level)
+    var underlyingError: Swift.Error? {
+        switch self {
+        case .imageMapping,
+             .jsonMapping,
+             .stringMapping,
+             .requestMapping,
+             .statusCode:
+            return nil
+        case .objectMapping(let error, _): 
+            return error
+        case .encodableMapping(let error):
+            return error
+        case .underlying(let error, _):
+            return error
+        case .parameterEncoding(let error):
+            return error
+        }
+    }
+
+    var isMappingError: Bool {
+        switch self {
+        case .objectMapping,
+             .encodableMapping,
+             .imageMapping,
+             .jsonMapping,
+             .stringMapping,
+             .requestMapping,
+             .parameterEncoding:
+            return true
+        case .statusCode,
+             .underlying:
+            return false
+        }
+    }
+}
+
+private extension Error {
+    var asMoyaError: MoyaError? {
+        return self as? MoyaError
     }
 }
