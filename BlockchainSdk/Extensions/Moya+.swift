01d2e2ea0291292ba314fe04ff1a103b60dda27d
Alexander Osokin <4981841+tureck1y@users.noreply.github.com>
2024-04-03 14:56:44 +0300

    Merge pull request #620 from tangem/feature/IOS-6263_polkadot_account_health_check
    IOS-6263: Polkadot account health check

--- BlockchainSdk/Extensions/Moya+.swift
+++ BlockchainSdk/Extensions/Moya+.swift
@@ -30,3 +30,66 @@ extension Moya.Task {
         return .requestJSONEncodable(jsonRPCParams)
     }
 }
+
+extension Moya.Response {
+    func tryMap<Output, Failure>(
+        output: Output.Type,
+        failure: Failure.Type,
+        using decoder: JSONDecoder = JSONDecoder(),
+        failsOnEmptyData: Bool = true
+    ) throws -> Output where Output: Decodable, Failure: Decodable, Failure: Error {
+        if let apiError = try? map(failure, using: decoder, failsOnEmptyData: failsOnEmptyData) {
+            throw apiError
+        }
+
+        return try map(output, using: decoder, failsOnEmptyData: failsOnEmptyData)
+    }
+}
+
+extension MoyaProvider {
+    func asyncRequest(for target: Target) async throws -> Response {
+        let cancellableWrapper = CancellableWrapper()
+
+        return try await withTaskCancellationHandler(
+            operation: { [weak self] in
+                return try await withCheckedThrowingContinuation { continuation in
+                    // This check is required since `operation` closure is called regardless of whether the task is cancelled or not
+                    if _Concurrency.Task.isCancelled {
+                        continuation.resume(throwing: CancellationError())
+                        return
+                    }
+
+                    cancellableWrapper.value = self?.request(target) { result in
+                        switch result {
+                        case .success(let responseValue):
+                            continuation.resume(returning: responseValue)
+                        case .failure(let error):
+                            continuation.resume(throwing: error)
+                        }
+                    }
+                }
+            },
+            onCancel: {
+                cancellableWrapper.cancel()
+            }
+        )
+    }
+}
+
+// MARK: - Auxiliary types
+
+/// Closures in `withTaskCancellationHandler(handler:operation:)` may be called on different threads,
+/// this wrapper provides required synchronization.
+private final class CancellableWrapper {
+    var value: Cancellable? {
+        get { criticalSection { innerCancellable } }
+        set { criticalSection { innerCancellable = newValue } }
+    }
+
+    private var innerCancellable: Cancellable?
+    private let criticalSection = Lock(isRecursive: false)
+
+    func cancel() {
+        criticalSection { innerCancellable?.cancel() }
+    }
+}
