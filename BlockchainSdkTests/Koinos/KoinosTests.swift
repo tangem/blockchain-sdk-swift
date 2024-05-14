6cabf01e68881843f7907ddad5cb6149d885fba2
Aleksei Muraveinik <amuraveinik@tangem.com>
2024-05-14 15:49:30 +0500

    IOS-6759: Add tests

---
+++ BlockchainSdkTests/Koinos/KoinosTests.swift
@@ -0,0 +1,34 @@
+//
+//  KoinosTests.swift
+//  BlockchainSdkTests
+//
+//  Created by Aleksei Muraveinik on 14.05.24.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import BitcoinCore
+@testable import BlockchainSdk
+import XCTest
+
+final class KoinosTests: XCTestCase {
+    private let addressService = KoinosAddressService(networkParams: BitcoinNetwork.mainnet.networkParams)
+
+    func testMakeAddress() {
+        let publicKey = "03B2D98CF41E82D9B99842A1D05860A1B06532015138F9067239706E06EE38E621"
+        let expectedAddress = "1AYz8RCnoafLnifMjJbgNb2aeW5CbZj8Tp"
+
+        XCTAssertEqual(try! addressService.makeAddress(from: publicKey.data(using: .hexadecimal)!).value, expectedAddress)
+    }
+    
+    func testValidateCorrectAddress() {
+        let address = "1AYz8RCnoafLnifMjJbgNb2aeW5CbZj8Tp"
+
+        XCTAssertTrue(addressService.validate(address))
+    }
+
+    func testValidateIncorrectAddress() {
+        let address = "1AYz8RCnoafLnifMjJbgNb2aeW5CbZj8T"
+
+        XCTAssertFalse(addressService.validate(address))
+    }
+}
