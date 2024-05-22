48a8a64b38a18e86f363a850b1d5a0dfe199b3c7
Aleksei Muraveinik <amuraveinik@tangem.com>
2024-05-22 13:41:03 +0400

    IOS-6909: KoinosTransactionBuilder

---
+++ BlockchainSdk/Blockchains/Koinos/KoinosTransactionBuilderError.swift
@@ -0,0 +1,13 @@
+//
+//  KoinosTransactionBuilderError.swift
+//  BlockchainSdk
+//
+//  Created by Aleksei Muraveinik on 22.05.24.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+enum KoinosTransactionBuilderError: Error {
+    case unableToParseParams
+}
