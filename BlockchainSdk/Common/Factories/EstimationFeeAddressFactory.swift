5324cff65518d89dc3676ac898d1d08f7dbdbf80
Dmitry Fedorov <80246944+fedorov-d@users.noreply.github.com>
2024-06-12 11:02:45 +0300

    IOS-6344 Use M-address for ltc estimation

--- BlockchainSdk/Common/Factories/EstimationFeeAddressFactory.swift
+++ BlockchainSdk/Common/Factories/EstimationFeeAddressFactory.swift
@@ -34,7 +34,7 @@ struct EstimationFeeAddressFactory {
         case .bitcoin:
             return "bc1qkrc5kmpq546wr2xk0errg58yw9jjq7thvhdk5k"
         case .litecoin:
-            return "LeAXZ4WKNy8zeybFkD6scFpBSmCPmENUEW"
+            return "MSqjXH6toL4kHqsRo3mWaWMkhmiH9GQxLR"
         case .bitcoinCash:
             return "bitcoincash:qrn96yyxa93t6sqmehvls6746qafkcsuku6zmd9460"
         case .dogecoin:
