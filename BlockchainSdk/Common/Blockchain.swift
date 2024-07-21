7e708b2fbb99c40b0b8c396b0a615401f5b03bf5
Aleksei Muraveinik <amuraveinik@tangem.com>
2024-07-21 23:00:43 +0200

    IOS-7228 Add coinDisplayName

--- BlockchainSdk/Common/Blockchain.swift
+++ BlockchainSdk/Common/Blockchain.swift
@@ -492,6 +492,14 @@ public indirect enum Blockchain: Equatable, Hashable {
             return name + testnetSuffix
         }
     }
+    
+    public var сoinDisplayName: String {
+        if case .ton = self {
+            "Toncoin"
+        } else {
+            displayName
+        }
+    }
 
     /// Provides a more descriptive display name for the fee currency (ETH) for some Ethereum L2s,
     /// for example: `'Optimistic Ethereum (ETH)'` instead of just `'ETH'`
